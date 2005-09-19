a081b293ec0c4e58e47e0ef4404c8fc2b54d4e26
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-09-19 19:31:15 +0000

    Skeleton

---
+++ dumbhippo/trunk/server/src/com/dumbhippo/server/DumbHippoServlet.java
@@ -0,0 +1,15 @@
+/**
+ * DumbHippo servlet that does some stuff we don't know yet
+ */
+package com.dumbhippo.server;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * DumbHippo servlet that does some stuff we don't know yet
+ * @author hp
+ *
+ */
+public class DumbHippoServlet extends HttpServlet {
+
+}
