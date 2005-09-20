e8ded270baabb8b57d0c31fa3580b79bc304dc7d
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-09-20 15:27:10 +0000

    Import plugin skeleton.

---
+++ dumbhippo/trunk/jiveplugin/com/dumbhippo/jive/LinkReflector.java
@@ -0,0 +1,24 @@
+package com.dumbhippo.jive;
+
+import org.jivesoftware.messenger.container.Plugin;
+import org.jivesoftware.messenger.container.PluginManager;
+
+import java.io.File;
+
+/**
+ * A sample plugin for Jive Messenger.
+ */
+public class LinkReflector implements Plugin {
+
+    public void initialize(PluginManager manager, File pluginDirectory) {
+    }
+
+    public void destroy() {
+    }
+
+	public void initializePlugin(PluginManager arg0, File arg1) {
+	}
+
+	public void destroyPlugin() {
+	}
+}
