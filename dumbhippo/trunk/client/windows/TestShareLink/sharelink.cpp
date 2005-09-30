283716c1bc9059ff907060e6c9c4ac1770b9c171
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-09-30 20:59:29 +0000

    Add a support for a <description/> child node of <link/>

--- dumbhippo/trunk/client/windows/TestShareLink/sharelink.cpp
+++ dumbhippo/trunk/client/windows/TestShareLink/sharelink.cpp
@@ -1,5 +1,6 @@
 #include <WinSock2.h>
 #include <loudmouth/loudmouth.h>
+#include <stdio.h>
 
 static bool
 initializeWinSock(void)
@@ -19,6 +20,88 @@ initializeWinSock(void)
     }
 
     return true;
+}
+
+static char *
+readDescription(void)
+{
+    GString *descriptionRaw = g_string_new(NULL);
+    g_print("Enter link description, finishing with a line with a single . character\n");
+    while (TRUE) {
+	char buf[1024];
+	if (!fgets(buf, G_N_ELEMENTS(buf), stdin))
+	    break;
+	if (strcmp(buf, ".") == 0 || strcmp(buf, ".\n") == 0)
+	    break;
+	g_string_append (descriptionRaw, buf);
+    }
+    char *description = g_locale_to_utf8(descriptionRaw->str, descriptionRaw->len, NULL, NULL, NULL);
+    g_string_free (descriptionRaw, TRUE);
+
+    if (description) {
+	g_strstrip(description);
+	if (strcmp (description, "") == 0) {
+	    g_free (description);
+	    description = NULL;
+	}
+    }
+
+    return description;
+}
+
+LmMessage *
+createMessage(char *to, char *link, char *title, char *description)
+{
+    LmMessage *message = lm_message_new_with_sub_type(to, 
+ 	                                              LM_MESSAGE_TYPE_MESSAGE,
+	                                              LM_MESSAGE_SUB_TYPE_HEADLINE);
+
+    lm_message_node_add_child(message->node, "subject", "New link");
+
+
+
+    GString *bodyString = g_string_new("Check out this link: ");
+
+    if (title)
+	g_string_append_printf(bodyString, "%s (%s). ", title, link);
+    else
+	g_string_append_printf(bodyString, "%s. ", link);
+
+    if (description)
+	g_string_append (bodyString, description);
+
+    lm_message_node_add_child(message->node, "body", bodyString->str);
+    g_string_free(bodyString, TRUE);
+
+    char *htmlLink;
+    htmlLink = g_markup_printf_escaped("<a href='%s'>%s</a>", link, title ? title : link);
+
+    char *htmlDescription;
+    if (description)
+	htmlDescription = g_markup_printf_escaped("<br/><p>%s</p>", description);
+    else
+	htmlDescription = g_strdup("");
+
+    char *htmlString;
+    htmlString = g_strdup_printf("<body xmlns='http://www.w3.org/1999/xmhtml'><p>Check out this link: %s</p>%s</body>", 
+	htmlLink, htmlDescription);
+
+    LmMessageNode *htmlNode = lm_message_node_add_child(message->node, "html", htmlString);
+    lm_message_node_set_attribute(htmlNode, "xmlns", "http://jabber.org/protocol/xhtml-im");
+    lm_message_node_set_raw_mode(htmlNode, TRUE);
+    g_free(htmlString);
+    g_free(htmlLink);
+    g_free(htmlDescription);
+
+    LmMessageNode *linkNode = lm_message_node_add_child(message->node, "link", NULL);
+    lm_message_node_set_attribute(linkNode, "xmlns", "http://dumbhippo.com/protocol/linkshare");
+    lm_message_node_set_attribute(linkNode, "href", link);
+    if (title)
+	lm_message_node_add_child(linkNode, "title", title);
+    if (description)
+	lm_message_node_add_child(linkNode, "description", description);
+
+    return message;
 }
 
 int 
@@ -58,7 +141,8 @@ main (int argc, char **argv)
 	return 1;  
     }
 
-    LmConnection *connection = lm_connection_new("messages.dumbhippo.com");
+    //LmConnection *connection = lm_connection_new("messages.dumbhippo.com");
+    LmConnection *connection = lm_connection_new("192.168.1.10");
     if (!lm_connection_open_and_block(connection, &error)) {
 	g_printerr("Couldn't connect to server: %s\n", error->message);
 	return 1;
@@ -68,44 +152,15 @@ main (int argc, char **argv)
 	return 1;
     }
 
+    char *description = readDescription();
+
     for (char **recipient = recipients; *recipient; recipient++) {
 	char *to = g_strdup_printf("%s@dumbhippo.com", *recipient);
 
-	LmMessage *message = lm_message_new_with_sub_type(to, 
-							  LM_MESSAGE_TYPE_MESSAGE,
-							  LM_MESSAGE_SUB_TYPE_HEADLINE);
-
-	lm_message_node_add_child(message->node, "subject", "New link");
-
-	gchar *bodyString;
-	if (title)
-	    bodyString = g_strdup_printf("Check out this link: %s (%s)", title, link);
-	else
-	    bodyString = g_strdup_printf("Check out this link: %s", link);
-
-	lm_message_node_add_child(message->node, "body", bodyString);
-	g_free(bodyString);
-
-	gchar *htmlString;
-	if (title)
-	    htmlString = g_strdup_printf("<body xmlns='http://www.w3.org/1999/xmhtml'><p>Check out this link: <a href='%s'>%s</a></p></body>", link, title);
-	else
-	    htmlString = g_strdup_printf("<body xmlns='http://www.w3.org/1999/xmhtml'><p>Check out this link: <a href='%s'>%s</a></p></body>", link, link);
-
-	LmMessageNode *htmlNode = lm_message_node_add_child(message->node, "html", htmlString);
-	lm_message_node_set_attribute(htmlNode, "xmlns", "http://jabber.org/protocol/xhtml-im");
-	lm_message_node_set_raw_mode(htmlNode, TRUE);
-	g_free(htmlString);
-
-	LmMessageNode *linkNode = lm_message_node_add_child(message->node, "link", NULL);
-	lm_message_node_set_attribute(linkNode, "xmlns", "http://dumbhippo.com/protocol/linkshare");
-	lm_message_node_set_attribute(linkNode, "href", link);
-	if (title)
-	    lm_message_node_add_child(linkNode, "title", title);
-
-	if (!lm_connection_send(connection, message, &error)) {
+	LmMessage *message = createMessage(to, link, title, description);
+	
+	if (!lm_connection_send(connection, message, &error))
 	    g_printerr("Error sending to %s: %s\n", to, error->message);
-	}
 	
 	g_free(to);
 	lm_message_unref(message);
