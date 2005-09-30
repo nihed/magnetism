33e3b9eb9b070063f4cd69634b0998ff35d2c8c3
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-09-30 23:00:10 +0000

    Make the plugin reflect links after munging them in pointless ways. Just proof of concept really.

--- dumbhippo/trunk/jiveplugin/src/java/com/dumbhippo/jive/HippoPlugin.java
+++ dumbhippo/trunk/jiveplugin/src/java/com/dumbhippo/jive/HippoPlugin.java
@@ -13,8 +13,6 @@ import org.xmpp.component.ComponentManagerFactory;
  * Our plugin for Jive Messenger
  */
 public class HippoPlugin implements Plugin {
-
-	private static final String linkReflectorDomain = "link-reflector";
 	
 	public void initializePlugin(PluginManager pluginManager, File pluginDirectory) {
 		Log.debug("Initializing Hippo plugin");
@@ -23,7 +21,7 @@ public class HippoPlugin implements Plugin {
 		ComponentManager componentManager = ComponentManagerFactory.getComponentManager();
 		
 		try {
-			componentManager.addComponent(linkReflectorDomain, new LinkReflectorComponent());
+			componentManager.addComponent(LinkReflectorComponent.DOMAIN, new LinkReflectorComponent());
 		} catch (ComponentException e) {
 			Log.error(e);
 		}
@@ -37,7 +35,7 @@ public class HippoPlugin implements Plugin {
 		ComponentManager componentManager = ComponentManagerFactory.getComponentManager();
 		
 		try {
-			componentManager.removeComponent(linkReflectorDomain);
+			componentManager.removeComponent(LinkReflectorComponent.DOMAIN);
 		} catch (ComponentException e) {
 			Log.error(e);
 		}
--- dumbhippo/trunk/jiveplugin/src/java/com/dumbhippo/jive/LinkReflectorComponent.java
+++ dumbhippo/trunk/jiveplugin/src/java/com/dumbhippo/jive/LinkReflectorComponent.java
@@ -8,6 +8,7 @@ import org.xmpp.component.ComponentException;
 import org.xmpp.component.ComponentManager;
 import org.xmpp.component.Log;
 import org.xmpp.packet.JID;
+import org.xmpp.packet.Message;
 import org.xmpp.packet.Packet;
 
 /**
@@ -16,28 +17,50 @@ import org.xmpp.packet.Packet;
  */
 public class LinkReflectorComponent implements Component {
 
-	private static final String name = "LinkReflectorComponent";
-	private static final String description = "Component used to notify clients of new shared links";
+	private static final String NAME = "LinkReflectorComponent";
+	private static final String DESCRIPTION = "Component used to notify clients of new shared links";
+
+	public static final String DOMAIN = "link-reflector";
 	
 	private Log logger = null;
+	private ComponentManager componentManager = null;
 	
 	public LinkReflectorComponent() {
 		org.jivesoftware.util.Log.debug("Constructing LinkReflectorComponent");
 	}
 	
 	public String getName() {
-		return name;
+		return NAME;
 	}
 
 	public String getDescription() {
-		return description;
+		return DESCRIPTION;
 	}
 
 	public void processPacket(Packet packet) {
 		logger.debug("LinkReflectorComponent processPacket(), from: " + packet.getFrom());
+		if (packet instanceof Message) {
+			Message message = (Message) packet;
+			logger.debug(message.toXML());
+			JID current = message.getTo();
+			String domain = current.getDomain();
+			logger.debug("  domain is " + domain);
+			String newDomain = domain.substring(DOMAIN.length()+1);
+			logger.debug("  new domain is " + newDomain);
+			packet.setTo(new JID(current.getNode(), newDomain, current.getResource()));
+			
+			packet.setFrom(new JID("TheMan", "dumbhippo.com", "system"));
+			
+			try {
+				componentManager.sendPacket(this, packet);
+			} catch (ComponentException e) {
+				logger.error(e);
+			}
+		}
 	}
 
 	public void initialize(JID jid, ComponentManager componentManager) throws ComponentException {
+		this.componentManager = componentManager;
 		logger = componentManager.getLog();
 		
 		logger.debug("Initializing LinkReflectorComponent, jid = " + jid);
