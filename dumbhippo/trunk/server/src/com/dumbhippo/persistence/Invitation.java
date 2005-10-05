ce79aede2008ff6bc57ca74805b7cea97fe44692
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-10-05 20:04:51 +0000

    Clean up imports.

--- dumbhippo/trunk/server/src/com/dumbhippo/persistence/Invitation.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/persistence/Invitation.java
@@ -5,7 +5,6 @@ import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
