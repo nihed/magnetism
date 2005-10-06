65ab21b1d1370906487b426208d94b2fff924173
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-10-06 23:14:44 +0000

    Improve starting Jive by writing a trivial raw-socket XMPP ping and redirecting output to /dev/null

--- dumbhippo/trunk/server/super/jive/files/scripts/jive-start.sh
+++ dumbhippo/trunk/server/super/jive/files/scripts/jive-start.sh
@@ -4,6 +4,8 @@ targetdir=@@targetdir@@
 mysqlTargetdir=@@mysqlTargetdir@@
 mysqlOptions=@@mysqlOptions@@
 
+echo "Starting Jive Messenger..."
+
 if [ -d $mysqlTargetdir/data/jive ] ; then : ; else
     /usr/bin/mysqladmin $mysqlOptions create jive
     /usr/bin/mysql $mysqlOptions jive < $targetdir/resources/database/messenger_mysql.sql
@@ -23,8 +25,37 @@ java -server \
      -DmessengerHome=$targetdir \
      -Dmessenger.lib.dir=$targetdir/lib \
      -classpath $targetdir/lib/startup.jar \
-     -jar $targetdir/lib/startup.jar &
+     -jar $targetdir/lib/startup.jar >/dev/null 2>&1 &
 
-echo "$!" > $targetdir/run/jive.pid
+pid=$!
 
+started=false
+failed=false
 
+timeout=30
+interval=1
+while [ $timeout -gt 0 ] ; do
+    if ps -p $pid > /dev/null ; then : ; else
+	failed=true
+	break
+    fi
+    if $targetdir/scripts/jive-running.py ; then
+	started=true
+	break
+    fi
+ 
+    sleep $interval
+    let timeout="$timeout - $interval"
+done
+
+if $started ; then
+    echo "$pid" > $targetdir/run/jive.pid
+    echo "...sucessfully started"
+    exit 0
+elif $failed ; then
+    echo "...failed to start"
+    exit 1
+else
+    echo "...timed out"
+    exit 1
+fi
