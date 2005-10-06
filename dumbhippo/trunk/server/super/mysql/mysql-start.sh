d9395832563fbb8a07b7c29089303a540b4fc19a
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-10-06 16:22:48 +0000

    * Move default deployment directory to ~/dhdeploy * Add basic build/start/stop functionality for MySQL

---
+++ dumbhippo/trunk/server/super/mysql/mysql-start.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+targetdir=$1
+
+echo "Starting MySQL..."
+/usr/bin/mysqld_safe --defaults-file=$targetdir/conf/my.cnf > /dev/null &
+pid=$!
+
+need_set_password=false
+if [ ! -d $targetdir/data/mysql ] ; then
+    echo "Creating database...."
+    /usr/bin/mysql_install_db --datadir=$targetdir/data > /dev/null
+
+    need_set_password=true
+fi
+
+started=false
+failed=false
+
+timeout=30
+interval=1
+while [ $timeout -gt 0 ] ; do
+    if ps -p $pid > /dev/null ; then : ; else
+	failed=true
+	break
+    fi
+    #'ping' works fine if there is no such user, but requires login if there 
+    # is a user. (Security leak? If this is fixed, we need to grep the
+    # response on failure)
+    if /usr/bin/mysqladmin -S $targetdir/run/mysql.sock -uUNKNOWN_MYSQL_USER ping > /dev/null 2>&1 ; then
+	started=true
+	break
+    fi
+ 
+    sleep $interval
+    let timeout="$timeout - $interval"
+done
+
+if $started ; then
+    if $need_set_password ; then
+	eval `grep 'password=' $targetdir/conf/my.cnf`
+	/usr/bin/mysqladmin -u root password $password
+    fi
+    echo "...sucessfully started"
+    exit 0
+elif $failed ; then
+    echo "...failed to start"
+    exit 1
+else
+    echo "...timed out"
+    exit 1
+fi
+
---
+++ dumbhippo/trunk/server/super/mysql/mysql-stop.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+targetdir=$1
+
+echo "Stopping MySQL..."
+
+if [ \! -f $targetdir/run/mysqld.pid ] ; then 
+    echo"... not running"
+    exit 0
+fi
+
+pid=`cat $targetdir/run/mysqld.pid`
+
+if kill $pid ; then : ; else
+    echo"... not running"
+    exit 0
+fi
+
+timeout=30
+interval=1
+while [ $timeout -gt 0 ] ; do
+    if ps -p $pid > /dev/null ; then : ; else
+	echo "... stopped"
+	exit 0
+    fi
+
+    sleep $interval
+    let timeout="$timeout - $interval"
+done
+
+echo "...timed out"
+exit 1
+
