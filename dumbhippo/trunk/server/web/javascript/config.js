159b4624f5bda6dc13109b5cb6d2a6655db73b4e
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-10-20 00:42:52 +0000

    Add some magic to support both file: and http: access

--- dumbhippo/trunk/server/web/javascript/config.js
+++ dumbhippo/trunk/server/web/javascript/config.js
@@ -1,16 +1,24 @@
 // config.js runs before we load dojo.js
 
-var dhServerUri = "http://127.0.0.1:8080/"
+var dhDebug = true;
+var dhServerUri = "/";
+var dhScriptRoot = dhServerUri + "javascript/";
+if (dhDebug && document.location.toString().substring(0,5) == "file:") {
+	// typical developer setup would use this
+	dhServerUri = "http://127.0.0.1:8080/";
+	dhScriptRoot = "javascript/";
+}
 var dhXmlRoot = dhServerUri + "xml/";
 var dhXmlRpcRoot = dhServerUri + "xmlrpc/";
 var dhTextRoot = dhServerUri + "text/";
 var dhPostRoot = dhServerUri + "action/";
 
-// configure Dojo
+// configure Dojo; don't put anything here we expect to change, instead make a 
+// dh variable above then slave djConfig to it.
 var djConfig = { 
-	isDebug: true,
+	isDebug: dhDebug,
 	preventBackButtonFix: true,
 //  this loads browser_debug.js which makes module loading a no-op?
 //	debugAtAllCosts: true,
-	baseScriptUri: "javascript/dojo/"
+	baseScriptUri: dhScriptRoot + "dojo/"
 };
