3104eb26ee9224cad5e74cc27b5bc4c82491b574
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-10-26 02:44:23 +0000

    Create database with character set utf8 collate utf8_bin, so that we get case-sensitive matching of strings

--- dumbhippo/trunk/server/super/jboss/files/scripts/jboss-start.sh
+++ dumbhippo/trunk/server/super/jboss/files/scripts/jboss-start.sh
@@ -12,7 +12,9 @@ echo "Starting jboss..."
 
 if [ -d $mysqlTargetdir/data/dumbhippo ] ; then : ; else
     echo "... dumbhippo database doesn't exist, creating ..."
-    /usr/bin/mysqladmin $mysqlOptions create dumbhippo
+    /usr/bin/mysql $mysqlOptions <<EOF
+create database dumbhippo character set utf8 collate utf8_bin
+EOF
 fi
 
 JAVA_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=$jdwpPort,suspend=n" \
--- dumbhippo/trunk/server/super/mysql/files/scripts/mysql-nuke.sh
+++ dumbhippo/trunk/server/super/mysql/files/scripts/mysql-nuke.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-targetdir=@@targetdir@@
-/usr/bin/mysql --defaults-file=$targetdir/conf/my.cnf <<EOF
+mysqlOptions=@@mysqlOptions@@
+
+/usr/bin/mysql $mysqlOptions <<EOF
 drop database dumbhippo
-create database dumbhippo
+create database dumbhippo character set utf8 collate utf8_bin
 EOF
 echo "database nuked; have a nice day!"
