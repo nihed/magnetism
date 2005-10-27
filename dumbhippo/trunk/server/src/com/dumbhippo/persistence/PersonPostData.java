e0bd4bb709f933106891d95bf5ee9912f0e3658b
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-10-27 19:04:13 +0000

    Add PersonPostData.

---
+++ dumbhippo/trunk/server/src/com/dumbhippo/persistence/PersonPostData.java
@@ -0,0 +1,50 @@
+package com.dumbhippo.persistence;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+// Not used yet
+//@Entity
+public class PersonPostData extends DBUnique {
+	private static final long serialVersionUID = 1L;
+	private Post post;
+	private Person person;
+	private Date clickedDate;
+	
+	public PersonPostData(Person person, Post post) {
+		this(person, post, new Date());
+	}
+	
+	public PersonPostData(Person person, Post post, Date clickedDate) {
+		this.person = person;
+		this.post = post;
+		this.clickedDate = clickedDate;
+	}
+	
+	public void setClickedDate(Date seenDate) {
+		this.clickedDate = seenDate;
+	}	
+
+	@OneToMany
+	public Person getPerson() {
+		return person;
+	}
+	@OneToMany
+	public Post getPost() {
+		return post;
+	}
+	@Column(nullable=true)
+	public Date getClickedDate() {
+		return clickedDate;
+	}
+	
+	protected void setPerson(Person person) {
+		this.person = person;
+	}
+	protected void setPost(Post post) {
+		this.post = post;
+	}	
+}
