c173db927f61e7c7a2dbec8ddf8ca13b8c806841
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-11-07 16:39:43 +0000

    Various cleanups.

--- dumbhippo/trunk/client/windows/Applets/notification.js
+++ dumbhippo/trunk/client/windows/Applets/notification.js
@@ -1,4 +1,8 @@
-getParamsFromLocation = function() {
+dh = {}
+dh.client = {}
+
+// Parse query parameters, sucked from dh.util.
+dh.client.getParamsFromLocation = function() {
 	var query = window.location.search.substring(1);
 	var map = {};
 	var params = query.split("&");
@@ -13,15 +17,28 @@ getParamsFromLocation = function() {
     return map;
 }
 
-handleLinkClicked = function(e) {
-  try {
-	if (!e) e = window.event;
-	e.cancelBubble = true; // e.stopPropagation();
-	e.returnValue = false;
-    window.external.LinkClicked();
-  } catch (e) {
-    alert(e);
-    return false;
+// Takes a DOM event handler function and fixes
+// the IE event object to have the standard DOM 2 functions etc.
+// Also wraps it in a try/catch for debugging purposes.
+dh.client.stdEventHandler = function(f) {
+  return function(e) {
+    try {
+      if (!e) e = window.event;
+      if (!e.stopPropagation) {
+        e.stopPropagation = function() { e.cancelBubble = true; }
+      }
+      e.returnValue = f(e);
+      return e.returnValue;
+    } catch (e) {
+      alert(e);
+      return false;
+    }
   }
-  return false;
 }
+
+// Called when the user clicks on the shared link
+dh.client.handleLinkClicked = dh.client.stdEventHandler(function(e) {
+	e.stopPropagation();
+	window.external.LinkClicked();
+	return false;
+})
