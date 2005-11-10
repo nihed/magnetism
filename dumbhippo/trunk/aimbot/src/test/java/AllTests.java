6c5f7d2fc683757f4ffc3d7898e5672342439e8c
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-11-10 03:06:03 +0000

    Initial junit and ant setup, not tested since I'm afraid I'll break it during testing ;-) will check in fixes momentarily

---
+++ dumbhippo/trunk/aimbot/src/test/java/AllTests.java
@@ -0,0 +1,11 @@
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+    public static Test suite() {
+        TestSuite suite = new TestSuite("All tests");
+        suite.addTest(new TestSuite(com.dumbhippo.aim.ScreenNameTest.class));
+        return suite;
+    }
+}
