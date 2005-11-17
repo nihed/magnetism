8961248b0d68ff424f184c6487aa628353a425af
clarkbw <clarkbw@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-11-17 18:33:01 +0000

    missed commit for framer page, added getChatRoom call with tabbing cleanups

--- dumbhippo/trunk/server/src/com/dumbhippo/web/FramerPage.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/web/FramerPage.java
@@ -1,3 +1,4 @@
+/* -*- tab-width: 4; indent-tabs-mode: t -*- */
 package com.dumbhippo.web;
 
 
@@ -17,35 +18,59 @@ import com.dumbhippo.server.IdentitySpider.GuidNotFoundException;
  */
 
 public class FramerPage {
-	static private final Log logger = GlobalSetup.getLog(FramerPage.class);	
+    static private final Log logger = GlobalSetup.getLog(FramerPage.class);	
 	
-	private String postId;
+    private String postId;
 
-	@Signin
-	private SigninBean signin;
+    @Signin
+		private SigninBean signin;
 	
-	private PostingBoard postBoard;
-	private PostView post;
+    private PostingBoard postBoard;
+    private PostView post;
+	private String chatroom;
 	
-	public FramerPage() {
+    public FramerPage() {
 		postBoard = WebEJBUtil.defaultLookup(PostingBoard.class);
-	}
+    }
 	
-	public SigninBean getSignin() {
+    public SigninBean getSignin() {
 		return signin;
-	}
+    }
 
-	public String getPostId() {
+    public String getPostId() {
 		return postId;
-	}
-	
-	protected void setPost(PostView post) {
+    }
+
+    public String getChatRoom() {
+		String title = this.post.getTitle();
+		String url = this.post.getUrl();
+		if  ( title == url ) {
+			// Get the domain name
+			try {
+				java.net.URL urlObject = new java.net.URL(url);
+				title = urlObject.getHost();
+			} catch (java.net.MalformedURLException e) {
+				title = "Dumb Hippo Chat";
+				logger.debug("Couldn't parse the URL object, created generic chat room: " + title);
+			}
+		}
+		// Get rid of spaces for '+'
+		title = title.replaceAll("[ \t\n\f\r]","+");
+		// Remove any other weird characters
+		title = title.replaceAll("[^a-zA-Z0-9\\+]","");
+		if (title.length() >= 10)
+			return title.substring(0, 10);
+		else 
+			return title;
+    }
+
+    protected void setPost(PostView post) {
 		this.post = post;
 		this.postId = post.getPost().getId();
 		logger.debug("viewing post: " + this.postId);
-	}
+    }
 
-	public void setPostId(String postId) throws ParseException, GuidNotFoundException {
+    public void setPostId(String postId) throws ParseException, GuidNotFoundException {
 		if (postId == null) {
 			logger.debug("no post id");
 			return;
@@ -53,9 +78,9 @@ public class FramerPage {
 			// Fixme - don't backtrace if the user isn't authorized to view the post
 			setPost(postBoard.loadPost(signin.getViewpoint(), new Guid(postId)));
 		}
-	}
+    }
 	
-	public PostView getPost() {
+    public PostView getPost() {
 		return post;
-	}
+    }
 }
