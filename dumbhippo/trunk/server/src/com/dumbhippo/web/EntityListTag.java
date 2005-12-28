4951daa87a2e2b78815711776b44d0970d683940
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-12-28 01:20:55 +0000

    Fix import warnings.
    Change bodyLengthLimit on people's names to be 10 instead of 4. Otherwise we get
    "H..." and "J..." instead of names.

--- dumbhippo/trunk/server/src/com/dumbhippo/web/EntityListTag.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/web/EntityListTag.java
@@ -3,7 +3,6 @@ package com.dumbhippo.web;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.el.ELException;
