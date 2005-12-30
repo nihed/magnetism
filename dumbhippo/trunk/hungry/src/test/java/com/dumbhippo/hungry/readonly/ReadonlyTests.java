b9cd28f56d147c8c90410d9366855a374f581336
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2005-12-30 22:39:59 +0000

    Fix the ant file

--- dumbhippo/trunk/hungry/src/test/java/com/dumbhippo/hungry/readonly/ReadonlyTests.java
+++ dumbhippo/trunk/hungry/src/test/java/com/dumbhippo/hungry/readonly/ReadonlyTests.java
@@ -7,7 +7,9 @@ import junit.framework.TestSuite;
 
 public class ReadonlyTests {
     public static Test suite() {
+    	System.out.println("Loading read-only test suite...");
         TestSuite suite = new PackageSuite("Read-only tests", ReadonlyTests.class);
+        System.out.println("... returning suite");
         return suite;
     }
 }
