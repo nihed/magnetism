493dc1ba9b92ae50ef2fe7a2c12b0865376f8a97
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-02-02 21:01:07 +0000

    Add nowplaying.jsp which gives you the html code for the flash embed Change nowPlaying.fla to have a click handler that goes to your music page, add names to the layers, some other stuff maybe.

---
+++ dumbhippo/trunk/server/src/com/dumbhippo/web/NowPlayingPage.java
@@ -0,0 +1,62 @@
+package com.dumbhippo.web;
+
+import com.dumbhippo.server.Configuration;
+import com.dumbhippo.server.HippoProperty;
+
+public class NowPlayingPage {
+
+	@Signin
+	private SigninBean signin;
+	
+	private Configuration config;
+	
+	public NowPlayingPage() {
+		config = WebEJBUtil.defaultLookup(Configuration.class);
+	}
+	
+	public SigninBean getSignin() {
+		return signin;
+	}
+
+	private String getBase() {
+		return config.getProperty(HippoProperty.BASEURL);
+	}
+	
+	private String getSrc() {
+		return getBase() + "/flash/nowPlaying.swf?who=" + signin.getUserId(); 
+	}
+	
+	private String getBgColor() {
+		return "#0099cc";
+	}
+	
+	public String getNowPlayingEmbedHtml() {
+		// try to keep this nicely formatted, since we give it to people to cut-and-paste
+		String format = ""
+		+ "<embed base=\"%s\"\n"
+		+ "       quality=\"high\" wmode=\"transparent\" bgcolor=\"%s\"\n"
+		+ "       width=\"440\" height=\"120\" name=\"nowPlaying\" align=\"middle\"\n"
+		+ "       allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\"\n"
+		+ "       pluginspage=\"http://www.macromedia.com/go/getflashplayer\"\n"
+		+ "       src=\"%s\" />\n";
+		return String.format(format, getBase(), getBgColor(), getSrc());
+	}
+	
+	public String getNowPlayingObjectHtml() {
+		// try to keep this nicely formatted, since we give it to people to cut-and-paste
+		String format = "" 
+		+ "<object classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\"\n"
+		+ "        codebase=\"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0\"\n"
+		+ "        width=\"440\" height=\"120\" align=\"middle\">\n"
+		+ "  <param name=\"allowScriptAccess\" value=\"sameDomain\" />\n"
+		+ "  <param name=\"base\" value=\"%s\" />\n"
+		+ "  <param name=\"quality\" value=\"high\" />\n"
+		+ "  <param name=\"wmode\" value=\"transparent\" />\n"
+		+ "  <param name=\"bgcolor\" value=\"%s\" />\n"
+		+ "  <param name=\"movie\" value=\"%s\" />\n"
+		+ "%s" // <embed> tag
+		+ "</object>\n";
+		
+		return String.format(format, getBase(), getBgColor(), getSrc(), getNowPlayingEmbedHtml());
+	}
+}
---
+++ dumbhippo/trunk/server/web/jsp/nowplaying.jsp
@@ -0,0 +1,55 @@
+<html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="dumbhippo.tld" prefix="dh" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="dht" %>
+
+<dh:bean id="nowplaying" class="com.dumbhippo.web.NowPlayingPage" scope="request"/>
+
+<head>
+        <title>Now Playing</title>
+        <dht:stylesheets />
+        <dht:scriptIncludes/>
+</head>
+<body>
+<div id="dhContainer">
+	<div id="dhMainArea">
+		<dht:logo/>
+
+		<dht:toolbar/>
+	
+		<h2>Now Playing</h2>
+		
+		<p>
+		&nbsp;
+		</p>
+		
+		<div>
+			<c:out value="${nowplaying.nowPlayingObjectHtml}" escapeXml="false"/>
+		</div>
+		
+		<h2>MySpace HTML</h2>
+		
+		<p>
+		To put the above "now playing" on MySpace, cut-and-paste this HTML:
+		</p>
+		
+		<div>
+			<!--  don't add whitespace inside the textarea tag -->
+			<textarea readonly="readonly" rows="7" wrap="off"><c:out value="${nowplaying.nowPlayingEmbedHtml}" escapeXml="true"/></textarea>
+		</div>
+
+		<h2>Alternative HTML</h2>
+		
+		<p>
+		The below HTML may improve browser compatibility, but won't work on MySpace.
+		</p>
+		
+		<div>
+			<!--  don't add whitespace inside the textarea tag -->
+			<textarea readonly="readonly" rows="17" wrap="off"><c:out value="${nowplaying.nowPlayingObjectHtml}" escapeXml="true"/></textarea>
+		</div>
+		
+	</div>
+</div>
+	
+</body>
