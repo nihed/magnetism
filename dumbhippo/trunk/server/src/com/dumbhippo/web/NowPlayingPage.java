589c9caec1436da255187bc5dff657739139cf6b
clarkbw <clarkbw@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-02-07 20:42:47 +0000

    add a little formatting to the embed

--- dumbhippo/trunk/server/src/com/dumbhippo/web/NowPlayingPage.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/web/NowPlayingPage.java
@@ -34,17 +34,22 @@ public class NowPlayingPage {
 	public String getNowPlayingEmbedHtml() {
 		// try to keep this nicely formatted, since we give it to people to cut-and-paste
 		String format = ""
+		+ "<div align=\"center\">\n"
+		+ "<strong>What I'm listening to right now</strong>\n"
 		+ "<embed quality=\"high\" wmode=\"transparent\" bgcolor=\"%s\"\n"
 		+ "       width=\"440\" height=\"120\" name=\"nowPlaying\" align=\"middle\"\n"
 		+ "       allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\"\n"
 		+ "       pluginspage=\"http://www.macromedia.com/go/getflashplayer\"\n"
-		+ "       src=\"%s\" />\n";
+		+ "       src=\"%s\" />\n"
+		+ "</div>\n";
 		return String.format(format, getBgColor(), getSrc());
 	}
 	
 	public String getNowPlayingObjectHtml() {
 		// try to keep this nicely formatted, since we give it to people to cut-and-paste
 		String format = "" 
+		+ "<div align=\"center\">\n"
+		+ "<strong>What I'm listening to right now</strong>\n"
 		+ "<object classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\"\n"
 		+ "        codebase=\"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0\"\n"
 		+ "        width=\"440\" height=\"120\" align=\"middle\">\n"
@@ -54,7 +59,8 @@ public class NowPlayingPage {
 		+ "  <param name=\"bgcolor\" value=\"%s\" />\n"
 		+ "  <param name=\"movie\" value=\"%s\" />\n"
 		+ "%s" // <embed> tag
-		+ "</object>\n";
+		+ "</object>\n"
+		+ "</div>\n";
 		
 		return String.format(format, getBgColor(), getSrc(), getNowPlayingEmbedHtml());
 	}
