4804bb132469c731cade31629a741dda5d2c7a31
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-05-18 18:12:57 +0000

    Remove unussed flickrstatus*

--- dumbhippo/trunk/client/windows/make-source-zip.sh
+++ dumbhippo/trunk/client/windows/make-source-zip.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PROJECT=Mugshot-windows
+
 DIRECTORIES="Applets dependencies HippoExplorer HippoShellExt HippoUI HippoUtil loudmouth TestLoudmouth WiXInstaller"
 
 version=`grep VERSION HippoUI/Version.h  | sed 's/[^"]*"\([^"]*\).*/\1/'`
@@ -9,6 +11,6 @@ IMAGE_TEST="-name '*.png'"
 NONIMAGE_FILES="`find $DIRECTORIES -name .svn -prune -o -name '*.dll' -o -name '*.lib' -o -name '*~' -o -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg' -o -print`"
 IMAGE_FILES="`find $DIRECTORIES -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg'`"
 
-rm -f DumbHippo-$version-source.zip
-zip -l DumbHippo-$version-source.zip $NONIMAGE_FILES
-zip DumbHippo-$version-source.zip $IMAGE_FILES
+rm -f $PROJECT-$version-source.zip
+zip -l $PROJECT-$version-source.zip $NONIMAGE_FILES
+zip $PROJECT-$version-source.zip $IMAGE_FILES
