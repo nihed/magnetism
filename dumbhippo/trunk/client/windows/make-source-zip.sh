91d2478e107bdf8e65a5214ac1356d3a5929104e
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-05-30 13:41:50 +0000

    Fix make-source-zip.sh for LICENSE => LICENSE.txt rename

--- dumbhippo/trunk/client/windows/make-source-zip.sh
+++ dumbhippo/trunk/client/windows/make-source-zip.sh
@@ -11,7 +11,7 @@ IMAGE_TEST="-name '*.png'"
 NONIMAGE_FILES="`find $DIRECTORIES -name .svn -prune -o -name '*.dll' -o -name '*.lib' -o -name '*~' -o -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg' -o -print`"
 IMAGE_FILES="`find $DIRECTORIES -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg'`"
 
-NONIMAGE_FILES="$NONIMAGE_FILES LICENSE"
+NONIMAGE_FILES="$NONIMAGE_FILES LICENSE.txt"
 
 rm -f $PROJECT-$version-source.zip
 zip -l $PROJECT-$version-source.zip $NONIMAGE_FILES
