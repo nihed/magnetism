87e1527a79f611f527c8c8757a5d529ebf4558b1
mpg <mpg@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-10-06 19:59:18 +0000

    Add cairo_surface_t

--- dumbhippo/trunk/client/canvas/python/arg-types.py
+++ dumbhippo/trunk/client/canvas/python/arg-types.py
@@ -13,3 +13,27 @@ class CairoParam(reversewrapper.Parameter):
         self.wrapper.add_pyargv_item("py_%s" % self.name)
 
 matcher.register_reverse("cairo_t*", CairoParam)
+
+class CairoSurfaceArg(ArgType):
+
+    before = ('    %(name)s = &((PycairoSurface*)(py_%(name)s))->surface;\n')
+
+    def write_param(self, ptype, pname, pdflt, pnull, info):
+        info.varlist.add('PyObject', '*py_' + pname)
+        info.varlist.add('cairo_surface_t', '*'+pname)
+        info.add_parselist('O', ['&py_'+pname], [pname])
+        info.arglist.append(pname)
+        info.codebefore.append (self.before % { 'name' : pname, 'namecopy' : 'NULL' })
+
+
+    def write_return(self, ptype, ownsreturn, info):
+        info.varlist.add('cairo_surface_t', '*ret')
+        info.codeafter.append('    if (ret)\n'
+                              '        return PycairoSurface_FromSurface(ret, NULL);\n'
+                              '    else {\n'
+                              '        Py_INCREF(Py_None);\n'
+                              '        return Py_None;\n'
+                              '    }');
+
+matcher.register('cairo_surface_t*', CairoSurfaceArg())
+
