caf83595aae6be830072844d53219115104ddc68
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-10-13 00:17:43 +0000

    Check hippo-canvas-type-builtins.[ch] into CVS so we can build on windows

---
+++ dumbhippo/trunk/client/common/hippo/generated/hippo-canvas-type-builtins.h
@@ -0,0 +1,52 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef HIPPO_CANVAS_TYPE_BUILTINS_H
+#define HIPPO_CANVAS_TYPE_BUILTINS_H 1
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+
+/* --- hippo/hippo-canvas-box.h --- */
+#define HIPPO_TYPE_PACK_FLAGS hippo_pack_flags_get_type()
+GType hippo_pack_flags_get_type (void);
+#define HIPPO_TYPE_CASCADE_MODE hippo_cascade_mode_get_type()
+GType hippo_cascade_mode_get_type (void);
+
+/* --- hippo/hippo-canvas-context.h --- */
+#define HIPPO_TYPE_STOCK_COLOR hippo_stock_color_get_type()
+GType hippo_stock_color_get_type (void);
+
+/* --- hippo/hippo-canvas-item.h --- */
+#define HIPPO_TYPE_CANVAS_POINTER hippo_canvas_pointer_get_type()
+GType hippo_canvas_pointer_get_type (void);
+#define HIPPO_TYPE_ITEM_ALIGNMENT hippo_item_alignment_get_type()
+GType hippo_item_alignment_get_type (void);
+
+/* --- hippo/hippo-canvas-text.h --- */
+#define HIPPO_TYPE_CANVAS_SIZE_MODE hippo_canvas_size_mode_get_type()
+GType hippo_canvas_size_mode_get_type (void);
+
+/* --- hippo/hippo-canvas-widgets.h --- */
+#define HIPPO_TYPE_SCROLLBAR_POLICY hippo_scrollbar_policy_get_type()
+GType hippo_scrollbar_policy_get_type (void);
+
+/* --- hippo/hippo-event.h --- */
+#define HIPPO_TYPE_EVENT_TYPE hippo_event_type_get_type()
+GType hippo_event_type_get_type (void);
+#define HIPPO_TYPE_MOTION_DETAIL hippo_motion_detail_get_type()
+GType hippo_motion_detail_get_type (void);
+
+/* --- hippo/hippo-graphics.h --- */
+#define HIPPO_TYPE_ORIENTATION hippo_orientation_get_type()
+GType hippo_orientation_get_type (void);
+#define HIPPO_TYPE_SIDE hippo_side_get_type()
+GType hippo_side_get_type (void);
+G_END_DECLS
+
+#endif /* HIPPO_CANVAS_TYPE_BUILTINS_H */
+
+/* Generated data ends here */
+
