e9d559c0c75b6c4943413b24216319a740725377
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-10-22 16:26:03 +0000

    add more alloc-fn to massif, not that it helps really

--- dumbhippo/trunk/client/linux/massif-mugshot.sh
+++ dumbhippo/trunk/client/linux/massif-mugshot.sh
@@ -10,6 +10,8 @@ G_SLICE=always-malloc G_DEBUG=gc-friendly \
    --alloc-fn=dbus_realloc --alloc-fn=g_strdup --alloc-fn=g_cclosure_new \
    --alloc-fn=g_closure_new_simple \
    --alloc-fn=FT_Alloc --alloc-fn=FT_QAlloc \
+   --alloc-fn=g_slice_alloc0 \
+   --alloc-fn=g_type_create_instance --alloc-fn=g_object_newv --alloc-fn=g_object_new \
   ./mugshot $*
 
 echo massif*
