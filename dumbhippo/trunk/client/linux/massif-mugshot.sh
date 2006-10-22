71118040fa8867910bdfd95da71f23affd62afb6
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-10-22 16:00:20 +0000

    script to run massif

---
+++ dumbhippo/trunk/client/linux/massif-mugshot.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+/bin/rm -f massif.*.ps massif.*.txt
+
+G_SLICE=always-malloc G_DEBUG=gc-friendly \
+   valgrind --tool=massif --depth=5 \
+   --alloc-fn=g_malloc --alloc-fn=g_malloc0 --alloc-fn=posix_memalign \
+   --alloc-fn=g_slice_alloc --alloc-fn=g_mem_chunk_alloc --alloc-fn=g_try_malloc \
+   --alloc-fn=dbus_malloc --alloc-fn=dbus_malloc0 --alloc-fn=g_realloc \
+   --alloc-fn=dbus_realloc --alloc-fn=g_strdup --alloc-fn=g_cclosure_new \
+   --alloc-fn=g_closure_new_simple \
+   --alloc-fn=FT_Alloc --alloc-fn=FT_QAlloc \
+  ./mugshot $*
+
+echo massif*
+
