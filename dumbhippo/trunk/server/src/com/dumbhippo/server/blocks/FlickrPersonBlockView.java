f6abc5703f72276b5cae5fe1318945d186144142
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-11-16 23:46:46 +0000

    Move Flickr/YouTube block handling into common classes.

--- dumbhippo/trunk/server/src/com/dumbhippo/server/blocks/FlickrPersonBlockView.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/server/blocks/FlickrPersonBlockView.java
@@ -1,20 +1,13 @@
 package com.dumbhippo.server.blocks;
 
-import com.dumbhippo.Thumbnails;
-import com.dumbhippo.XmlBuilder;
 import com.dumbhippo.persistence.Block;
 import com.dumbhippo.persistence.ExternalAccountType;
 import com.dumbhippo.persistence.GroupBlockData;
 import com.dumbhippo.persistence.UserBlockData;
-import com.dumbhippo.server.views.ExternalAccountView;
-import com.dumbhippo.server.views.PersonView;
 import com.dumbhippo.server.views.Viewpoint;
 
-public class FlickrPersonBlockView extends AbstractPersonBlockView
-	implements ExternalAccountBlockView, ThumbnailsBlockView, SimpleTitleBlockView {
+public class FlickrPersonBlockView extends ExternalThumbnailedPersonBlockView {
 
-	private ExternalAccountView externalAccountView;
-	
 	public FlickrPersonBlockView(Viewpoint viewpoint, Block block, UserBlockData ubd, boolean participated) {
 		super(viewpoint, block, ubd, participated);
 	}
@@ -23,55 +16,21 @@ public class FlickrPersonBlockView extends AbstractPersonBlockView
 		super(viewpoint, block, gbd, participated);
 	}
 
-	@Override
-	public String getIcon() {
-		return "/images3/" + ExternalAccountType.FLICKR.getIconName();
-	}
-
-	@Override
-	protected void writeDetailsToXmlBuilder(XmlBuilder builder) {
-		builder.openElement("flickrPerson",
-							"userId", getUserView().getUser().getId());
-		writeThumbnailsToXmlBuilder(builder, this);
-		builder.closeElement();
-	}
-
-	void populate(PersonView userView, ExternalAccountView externalAccountView) {
-		super.partiallyPopulate(userView);
-		this.externalAccountView = externalAccountView;
-		setPopulated(true);
-	}
-	
 	public ExternalAccountType getAccountType() {
 		return ExternalAccountType.FLICKR;
-	}
-
-	public Thumbnails getThumbnails() {
-		return externalAccountView.getThumbnails();
-	}
-
-	public String getMoreThumbnailsLink() {
-		return externalAccountView.getLink();
-	}
-
-	public String getMoreThumbnailsTitle() {
-		return externalAccountView.getExternalAccount().getLink();
-	}
-
-	public String getTitleForHome() {
-		return getTitle();
 	}
 	
 	public String getTitle() {
 		return "New photos posted";
 	}
 
-	public String getLink() {
-		return getMoreThumbnailsLink(); 
-	}
-
 	@Override
 	public String getTypeTitle() {
 		return "Flickr photos";
+	}
+
+	@Override
+	protected String getElementName() {
+		return "flickrPerson";
 	}
 }
--- dumbhippo/trunk/server/web/tags/3/youTubePersonBlock.tag
+++ dumbhippo/trunk/server/web/tags/3/youTubePersonBlock.tag
@@ -9,14 +9,4 @@
 <%@ attribute name="showFrom" required="false" type="java.lang.Boolean" %>
 <%@ attribute name="oneLine" required="true" type="java.lang.Boolean" %>
 
-<dht3:blockContainer cssClass="${offset ? 'dh-box-grey2' : 'dh-box-grey1'}" blockId="${blockId}">
-	<dht3:blockLeft block="${block}">
-		<dht3:simpleBlockTitle block="${block}" oneLine="${oneLine}" homeStack="false"/>
-		<dht3:blockDescription blockId="${blockId}" literalBody="true">
-			<dht3:blockThumbnails block="${block}"/>
-		</dht3:blockDescription>			
-	</dht3:blockLeft>
-	<dht3:blockRight blockId="${blockId}" from="${block.personSource}" showFrom="${showFrom}">
-		<dht3:blockTimeAgo blockId="${blockId}" block="${block}"/>
-	</dht3:blockRight>
-</dht3:blockContainer>
+<dht3:externalThumbnailedPersonBlock block="${block}" offset="${offset}" blockId="${blockId}" showFrom="${showFrom}" oneLine="${oneLine}"/>
