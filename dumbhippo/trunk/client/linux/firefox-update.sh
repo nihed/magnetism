e51ea70579594e4e0da634723fce7d257cd90e73
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-11-27 17:12:51 +0000

    Fix firefox-update.sh to handle /usr/lib64 (#993, Trond Danielsen)

--- dumbhippo/trunk/client/linux/firefox-update.sh
+++ dumbhippo/trunk/client/linux/firefox-update.sh
@@ -3,23 +3,36 @@
 # This script is used to add and remove our extension from the Firefox
 # directory, and is run from 'triggers' when Firefox is installed or
 # upgraded, as well as when our package is installed. It is needed because
-# Firefox is installed into versioned directories in /usr/lib/firefox
+# Firefox is installed into versioned directories in /usr/lib[64]/firefox
 #
 if [ "$1" = "install" ] ; then
-    # Add symlinks to any firefox directory that looks like it is part of a
-    # currently installed package
-    for d in /usr/lib/firefox* ; do
-	if [ -e $d/firefox-bin -a -d $d/extensions -a ! -L $d/extensions/firefox@mugshot.org ] ; then
-	    ln -s /usr/lib/mugshot/firefox $d/extensions/firefox@mugshot.org
-	fi
+    for libdir in /usr/lib /usr/lib64 ; do
+	# Add symlinks to any firefox directory that looks like it is part of a
+	# currently installed package
+	for d in $libdir/firefox* ; do
+	    if [ d = "$libdir/firefox*" ] ; then
+		continue
+	    fi
+	    link=$d/extensions/firefox@mugshot.org
+	    target=$libdir/mugshot/firefox
+	    if [ -e $target -a -e $d/firefox-bin -a -d $d/extensions -a ! -L $link ] ; then
+		ln -s $target $link
+	    fi
+	done
     done
 elif [ "$1" = "remove" ] ; then
-    # Remove any symlinks we've created into any firefox directory
-    for d in /usr/lib/firefox* ; do
-	if [ -L $d/extensions/firefox@mugshot.org ] ; then
-	    rm $d/extensions/firefox@mugshot.org
-	fi
-    done
+    for libdir in /usr/lib /usr/lib64 ; do
+	# Remove any symlinks we've created into any firefox directory
+	for d in $libdir/firefox* ; do
+	    if [ d = "$libdir/firefox*" ] ; then
+		continue
+	    fi
+	    link=$d/extensions/firefox@mugshot.org
+	    if [ -L $link ] ; then
+		rm $link
+	    fi
+	done
+     done
 else
     echo "Usage firefox-update.sh [install/remove]"
 fi
