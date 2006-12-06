dd120876e6a05fcf78592612aea730f0c0e1bf13
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-12-06 20:09:59 +0000

    a little nicer error message on problems with enumIs (helps debug bad db migrations...)

--- dumbhippo/trunk/server/src/com/dumbhippo/web/tags/FuncUtils.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/web/tags/FuncUtils.java
@@ -2,6 +2,10 @@ package com.dumbhippo.web.tags;
 
 public class FuncUtils {
 	public static boolean enumIs(Enum value, String name) {
+		if (value == null)
+			throw new IllegalArgumentException("null enum value passed to enumIs function, name=" + name);
+		if (name == null)
+			throw new IllegalArgumentException("null name passed to enumIs function, value=" + value);
 		return value.name().equals(name);
 	}
 	
