734ff3dabf77ff05e0c12d48a67d8085ff4b821e
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-12-08 21:42:55 +0000

    Default the 'scheme' attribute of the <rating/> tag to urn:simple as per the spec (#1000)

--- dumbhippo/trunk/mediarss/src/java/com/sun/syndication/feed/module/mediarss/io/MediaModuleParser.java
+++ dumbhippo/trunk/mediarss/src/java/com/sun/syndication/feed/module/mediarss/io/MediaModuleParser.java
@@ -322,8 +322,10 @@ public class MediaModuleParser implements ModuleParser {
             for (int i = 0; (ratings != null) && (i < ratings.size()); i++) {
                 try {
                     Element rat = (Element) ratings.get(i);
-                    values.add(new Rating(rat.getAttributeValue("scheme"),
-                            rat.getText()));
+                    String scheme = rat.getAttributeValue("scheme");
+                    if (scheme == null)
+                    	scheme = "urn:simple";
+                    values.add(new Rating(scheme, rat.getText()));
                 } catch (Exception ex) {
                     LOG.log(Level.WARNING, "Exception parsing rating tag.", ex);
                 }
