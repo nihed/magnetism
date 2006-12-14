4602573bc361c051bd2f3ae8068e99d52bd2364a
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-12-14 22:00:02 +0000

    all block views already have getReferencedObjects so we don't need it in EntitySourceBlockView

--- dumbhippo/trunk/server/src/com/dumbhippo/server/blocks/EntitySourceBlockView.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/server/blocks/EntitySourceBlockView.java
@@ -1,12 +1,8 @@
 package com.dumbhippo.server.blocks;
 
-import java.util.List;
-
 import com.dumbhippo.server.views.EntityView;
 
 public interface EntitySourceBlockView {
-	
-	public List<Object> getReferencedObjects();
 	
 	public EntityView getEntitySource();
 }
