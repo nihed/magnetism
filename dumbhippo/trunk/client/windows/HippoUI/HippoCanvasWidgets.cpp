beb498063026a762ceb86a16671cf232e3404e90
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-12-14 21:28:35 +0000

    Add (non-compiling) item wrapper code for an entry control

--- dumbhippo/trunk/client/windows/HippoUI/HippoCanvasWidgets.cpp
+++ dumbhippo/trunk/client/windows/HippoUI/HippoCanvasWidgets.cpp
@@ -7,6 +7,7 @@
 #include <hippo/hippo-canvas-widgets.h>
 #include "HippoCanvasControl.h"
 #include "HippoCanvas.h"
+#include "HippoEntryControl.h"
 
 #define HIPPO_DEFINE_CONTROL_ITEM(lower, Camel)                                         \
     struct _HippoCanvas##Camel { HippoCanvasControl parent; };                          \
@@ -76,10 +77,138 @@ hippo_canvas_scrollbars_set_policy (HippoCanvasScrollbars *scrollbars,
     scrollbars_get_control(scrollbars)->setScrollbarPolicy(orientation, policy);
 }
 
+/*************************************************************************/
+
+class HippoCanvasEntryListener : HippoEntryControlListener {
+public:
+    HippoCanvasEntryListener(HippoCanvasItem *item, HippoEntryControl *control) {
+        item_ = item;
+        control_ = control;
+    }
+
+    virtual void onTextChanged();
+    virtual bool onKeyPress(HippoKey key, gunichar character);
+
+private:
+    HippoCanvasItem *item_;
+    HippoEntryControl *control_;
+}
+
+struct _HippoCanvasEntry {
+    HippoCanvasControl parent;
+
+    HippoCanvasEntryListener *listener;
+};
+
+struct _HippoCanvasEntryClass { 
+    HippoCanvasControlClass parent; 
+};
+
+G_DEFINE_TYPE(HippoCanvasEntry, hippo_canvas_entry, HIPPO_TYPE_CANVAS_CONTROL)
+
+static void 
+hippo_canvas_entry_init(HippoCanvasEntry *canvas_entry) {
+}
+
+HippoEntryControl *
+entry_get_control(HippoCanvasEntry *canvas_entry)
+{
+    HippoAbstractControl *control = HIPPO_CANVAS_CONTROL(canvas_entry)->control;
+    g_assert(control != NULL);
+    
+    return (HippoEntryControl *)control;
+}
+
+static void
+hippo_canvas_entry_dispose(GObject *object)
+{
+    HippoCanvasEntry *canvas_entry = HIPPO_CANVAS_ENTRY (object);
+    HippoEntryControl *entry = entry_get_control(canvas_entry);
+
+    if (entry)
+        entry->setListener(null);
+
+    G_OBJECT_CLASS(hippo_canvas_entry_parent_class)->dispose(object);
+}
+
+static void
+hippo_canvas_entry_set_property(GObject        *object,
+                                guint            prop_id,
+                                const GValue    *value,
+                                GParamSpec      *pspec)
+{
+    HippoCanvasEntry *canvas_entry = HIPPO_CANVAS_ENTRY(object);
+    HippoEntryControl *entry = entry_get_control(canvas_entry);
+
+    switch (prop_id) {
+    case ENTRY_PROP_TEXT:
+        HippoBSTR str = HippoBSTR.fromUTF8(g_value_get_string(value));
+        entry->setText(str);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
+}
+
+static void
+hippo_canvas_entry_get_property(GObject        *object,
+                                guint            prop_id,
+                                GValue          *value,
+                                GParamSpec      *pspec)
+{
+    HippoCanvasEntry *canvas_entry = HIPPO_CANVAS_ENTRY (object);
+    HippoEntryControl *entry = entry_get_control(canvas_entry);
+
+    switch (prop_id) {
+    case ENTRY_PROP_TEXT:
+        HippoUStr ustr(entry->getText());
+        g_value_set_string(value, ustr.c_str);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
+}
+
+static void
+hippo_canvas_entry_class_init(HippoCanvasEntryClass *class)
+{
+    GObjectClass *object_class = G_OBJECT_CLASS(class);
+
+    object_class->dispose = hippo_canvas_entry_dispose;
+    object_class->set_property = hippo_canvas_entry_set_property;
+    object_class->get_property = hippo_canvas_entry_get_property;
+    
+    g_object_class_install_property(object_class,
+                                    ENTRY_PROP_TEXT,
+                                    g_param_spec_string("text",
+                                                        _("Text"),
+                                                        _("Text in the entry"),
+                                                        NULL,
+                                                        G_PARAM_READABLE | G_PARAM_WRITABLE));
+}
+
 HippoCanvasItem*
 hippo_canvas_entry_new(void)
 {
-    return HIPPO_CANVAS_ITEM(g_object_new(HIPPO_TYPE_CANVAS_ENTRY,
-                            "control", NULL, /* FIXME */
-                            NULL));
+    HippoEntryControl *entry = new HippoEntryControl();
+    HippoCanvasEntry *item = g_object_new(HIPPO_TYPE_CANVAS_ENTRY,
+                                          "control", entry,
+                                          NULL);
+
+    item->listener = new HippoCanvasEntryListener(HIPPO_CANVAS_ITEM(item), entry);
+    entry->setListener(item->listener);
+
+    return HIPPO_CANVAS_ITEM(item);
+}
+
+void 
+HippoCanvasEntryListener::onTextChanged() {
+    g_object_notify(G_OBJECT(item_), "text");
+}
+
+bool 
+HippoCanvasEntryListener::onKeyPress(HippoKey key, gunichar character) {
+    return hippo_canvas_item_emit_key_press_event(item_, key, character) != FALSE;
 }
