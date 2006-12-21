035fe1218412c98e8520ee65e0d8bca751f74625
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2006-12-21 19:48:27 +0000

    change invite mail subject to Mugshot Invitation instead of Mugshot Download and remove a message that didn't make much sense anymore

--- dumbhippo/trunk/server/src/com/dumbhippo/server/impl/HttpMethodsBean.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/server/impl/HttpMethodsBean.java
@@ -874,7 +874,7 @@ public class HttpMethodsBean implements HttpMethods, Serializable {
 					// this does NOT check whether the account has invitations left,
 					// that's why we do it above.
 					note = invitationSystem.sendEmailInvitation(new UserViewpoint(inviter), promotionCode, address,
-								"Mugshot Download", "Hey!\n\nClick here to get the Mugshot Music Radar and Web Swarm.");
+								"Mugshot Invitation", "");
 					if (note == null)
 						note = "Your invitation is on its way (check your email)";
 				}
