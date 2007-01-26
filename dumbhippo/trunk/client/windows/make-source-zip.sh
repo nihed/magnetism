d98ae419edb84cd9f14c93c5c2145d0109dffa14
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-01-26 21:31:10 +0000

    Quick update make-source-zip.sh; needs rewriting from scratch

--- dumbhippo/trunk/client/windows/make-source-zip.sh
+++ dumbhippo/trunk/client/windows/make-source-zip.sh
@@ -2,13 +2,13 @@
 
 PROJECT=Mugshot
 
-DIRECTORIES="dependencies HippoExplorer HippoShellExt HippoUI HippoUtil Images loudmouth TestLoudmouth WiXInstaller"
+DIRECTORIES="airbag dependencies cairo HippoExplorer HippoFirefox HippoFirefoxExtension HippoFirefoxStub HippoIpc HippoShellExt HippoUI HippoUtil libglib libgmodule libgobject libgthread libpango libpangocairo libpangowin32 loudmouth Images loudmouth Sheets util WiXInstaller"
 
 version=`grep VERSION HippoUI/Version.h  | sed 's/[^"]*"\([^"]*\).*/\1/'`
 
 IMAGE_TEST="-name '*.png'"
 
-NONIMAGE_FILES="`find $DIRECTORIES -name .svn -prune -o -name '*.dll' -o -name '*.dll.a' -o -name '*.lib' -o -name '*~' -o -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg' -o -print`"
+NONIMAGE_FILES="`find $DIRECTORIES -name .svn -prune -o -name '*.dll' -o -name '*.dll.a' -o -name '*.exe' -o -name '*.lib' -o -name '*~' -o -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg' -o -print`"
 IMAGE_FILES="`find $DIRECTORIES -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg'`"
 
 NONIMAGE_FILES="$NONIMAGE_FILES LICENSE.txt"
@@ -20,7 +20,6 @@ zip $PROJECT-$version-source.zip $IMAGE_FILES
 # Add in a common/ subdirectory; this isn't in the right place for the project
 # files, but people will probably be building out of subversion anyways
 cd ..
-( cd common && make clean ) 
-NONIMAGE_FILES="`find common -name .svn -prune -o -name '*.dll' -o -name '*.dll.a' -o -name '*.lib' -o -name '*~' -o -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg' -o -print`"
+NONIMAGE_FILES="`find common/firefox common/hippo common/hippoipc common/images -name .svn -prune -name '*.dll' -o -name '*.dll.a' -o -name '*.exe' -o -name '*.lib' -o -name '*~' -o -name '*.png' -o -name '*.bmp' -o -name '*.ico' -o -name '*.jpg' -o -print`"
 zip -l windows/$PROJECT-$version-source.zip $NONIMAGE_FILES
 
