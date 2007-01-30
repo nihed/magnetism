45e5e6c1f9d095f74c55a9eae5aabb89b1d123d2
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-01-30 17:32:58 +0000

    Makefile-libhippofirefox.am: Dist some missing gecko-private headers chatWindow.js: Hook up event handlers so that Escape closes the window   and so that links open in a new window (#1154)

--- dumbhippo/trunk/client/common/firefox/extension/chrome/content/chatWindow.js
+++ dumbhippo/trunk/client/common/firefox/extension/chrome/content/chatWindow.js
@@ -1,30 +1,53 @@
 var browser;
 
 function getParamsFromLocation() {
-	var query = window.location.search.substring(1);
-	var map = {};
-	var params = query.split("&");
-   	for (var i = 0; i < params.length; i++) {
-   		var eqpos = params[i].indexOf('=')
-   		if (eqpos > 0) {
-   		    var key = params[i].substring(0, eqpos);
-   		    var val = params[i].substring(eqpos+1);
-   		    // Java encodes spaces as +'s, we need to change that
-   		    // into something that decodeURIComponent can understand
-   		    val = val.replace(/\+/g, "%20");
-   		    map[key] = decodeURIComponent(val);
-   		}
+        var query = window.location.search.substring(1);
+        var map = {};
+        var params = query.split("&");
+        for (var i = 0; i < params.length; i++) {
+                var eqpos = params[i].indexOf('=')
+                if (eqpos > 0) {
+                    var key = params[i].substring(0, eqpos);
+                    var val = params[i].substring(eqpos+1);
+                    // Java encodes spaces as +'s, we need to change that
+                    // into something that decodeURIComponent can understand
+                    val = val.replace(/\+/g, "%20");
+                    map[key] = decodeURIComponent(val);
+                }
     }
     return map;
 }
 
+function onContentDOMActivate(e) {
+    var browser = document.getElementById("content");
+
+   if (e.target.tagName.toLowerCase() == "a") {
+      var a = e.target;
+      e.preventDefault();
+      e.stopPropagation();
+      browser.contentWindow.open(a.href, "_blank");
+   }
+}
+
+function onContentKeypress(e) {
+   if (e.keyCode == 27) {
+      var a = e.target;
+      e.preventDefault();
+      e.stopPropagation();
+      window.close();
+   }
+}
+
 function init() {
-    browser = document.getElementById("content");
+    var browser = document.getElementById("content");
+
+    browser.addEventListener("keypress", onContentKeypress, true);
+    browser.addEventListener("DOMActivate", onContentDOMActivate, true);
 
     var params = getParamsFromLocation();
 
     if (params["src"]) {
-	const nsIWebNavigation = Components.interfaces.nsIWebNavigation;
+        const nsIWebNavigation = Components.interfaces.nsIWebNavigation;
         browser.webNavigation.loadURI(params["src"], nsIWebNavigation.LOAD_FLAGS_NONE, null, null, null);
     }
 }
