941068f20d772ca7a6d99a603478b8e64ae5d34d
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-03-07 23:23:10 +0000

    Avoid trying to create our own UI for deskbar - just stuff the thing as-is into bigboard for now.  This turned out to be shockingly easy.

--- dumbhippo/trunk/client/bigboard/src/stocks/search_stock.py
+++ dumbhippo/trunk/client/bigboard/src/stocks/search_stock.py
@@ -1,11 +1,9 @@
 import logging, os
 
-import gobject
+import gobject, gtk, gnomeapplet
 
 try:
-    import deskbar, deskbar.ui
-    from deskbar.ModuleList import ModuleList
-    from deskbar.ModuleLoader import ModuleLoader
+    import deskbar, deskbar.DeskbarApplet
     deskbar_available = True    
 except:
     deskbar_available = False
@@ -16,108 +14,6 @@ import identity_spider, mugshot
 import libbig
 from bigboard import AbstractMugshotStock
 from big_widgets import CanvasMugshotURLImage, CanvasEntry
-
-class DeskbarAPI(gobject.GObject):
-    """Wrapper class for programatically searching via Deskbar.
-       * Hides the difference between sync and async modules - everything is async.
-       * Doesn't directly depend on deskbar UI or GConf, though it probably does indirectly.
-       * Deals with initializing modules, ignoring disabled ones, etc.  All you have
-         to do is ask it to search.
-       
-       Based on DeskbarApplet.py from deskbar.
-    """
-    __gsignals__ = {
-        "initialized" : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ())
-    }    
-    
-    def __init__(self):
-        gobject.GObject.__init__(self)
-
-        self.__logger = logging.getLogger('bigboard.stocks.SearchStock')
-        
-        self.__module_list = []
-        
-        self.__initialized_module_count = 0
-        self.__loaded_module_count = 0
-        
-        self.__loader = ModuleLoader(deskbar.MODULES_DIRS)
-        self.__loader.connect("modules-loaded", self.__on_modules_loaded)
-                        
-        self.__loader.connect("module-loaded", self.__on_module_loaded)
-        self.__loader.connect("module-initialized", self.__on_module_initialized)
-        self.__loader.connect("module-not-initialized", self.__on_module_initialized) # er, what?
-        
-        self.__enabled_module_names = ['ProgramsHandler', 'FileFolderHandler']
-        
-        self.__loader.load_all_async()        
-        
-    def set_enabled_modules(self, enabled):
-        self.__enabled_module_names = enabled
-    
-    def __on_module_initialized(self, sender, context):
-        self.__logger.debug("module %s initalized", context)
-        self.__initialized_module_count += 1
-        if context.module.is_async():
-            context.module.connect ('query-ready', 
-                                    lambda sender, qstring, matches: self.__handle_matches(self.__filter_matches([(qstring, match) for match in matches])))
-        if self.__initialized_module_count == self.__loaded_module_count:
-            self.emit("initialized")            
-    
-    def __get_module_by_name(self, name):
-        for module in self.__module_list:
-            if module.handler == name:
-                return module
-        return None
-    
-    def __on_module_loaded(self, sender, context):
-        self.__logger.debug("loaded module %s", context)
-        self.__module_list.append(context)
-    
-    def __on_modules_loaded(self, loader):
-       for modname in self.__enabled_module_names:
-           module = self.__get_module_by_name(modname)
-           self.__logger.debug("async initializing module %s", modname)
-           self.__loader.initialize_module_async(module)
-           self.__loaded_module_count += 1
-    
-    def query(self, qstring, cb):
-        self.__match_hashes = {}
-        self.__qstring = qstring
-        self.__cb = cb
-        results = []
-        for modctx in self.__module_list:
-            if not modctx.enabled:
-                    continue
-            if modctx.module.is_async():
-                    modctx.module.query_async(qstring)
-            else:
-                try:
-                    matches = modctx.module.query(qstring)
-                except TypeError:
-                    matches = modctx.module.query(qstring, deskbar.DEFAULT_RESULTS_PER_HANDLER)               
-                results.extend(self.__filter_matches(qstring, matches))
-        
-        if len(results) > 0:
-            gobject.idle_add(lambda: self.__handle_matches(results))
-            
-    def __filter_matches(self, qstring, matches):
-        results = []
-        for match in matches:
-            text, match = qstring, match
-            if type(match) is tuple:
-                text, match = match
-
-            hsh = match.get_hash(text)
-            if hsh != None:
-                if hsh in self.__match_hashes:
-                    continue        
-                results.append((text,match))
-            else:
-                results.append((text,match))    
-        return results           
-            
-    def __handle_matches(self, matches):
-        self.__cb(self.__qstring, matches)
         
 class SearchStock(AbstractMugshotStock):
     """Search.  It's what's for dinner."""
@@ -132,12 +28,15 @@ class SearchStock(AbstractMugshotStock):
             self.__box.append(hippo.CanvasText(text="Deskbar not installed"))
             return
         
-        self.__deskbar = DeskbarAPI()
+        self.__vbox = gtk.VBox()
+        self.__widget = hippo.CanvasWidget(widget=self.__vbox)
         
-        self.__input = CanvasEntry()
-        self.__input.connect("notify::text", lambda *args: self.__queue_search())
+        self.__applet = gnomeapplet.Applet()
+        self.__applet.get_orient = lambda: gnomeapplet.ORIENT_DOWN
+        deskbar.DeskbarApplet.DeskbarApplet(self.__applet)        
+        self.__applet.reparent(self.__vbox)
         
-        self.__box.append(self.__input)
+        self.__box.append(self.__widget)
         
     def _on_mugshot_initialized(self):
         super(SearchStock, self)._on_mugshot_initialized()
