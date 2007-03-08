03637b65aef21cc22f9ce46ed6a13e0f89583c5a
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-03-08 00:08:29 +0000

    stubbing out calendar feed parser

--- dumbhippo/trunk/client/bigboard/src/google.py
+++ dumbhippo/trunk/client/bigboard/src/google.py
@@ -1,4 +1,37 @@
-import httplib2, keyring, libbig, sys
+import httplib2, keyring, libbig, sys, xml, xml.sax
+
+class Event(libbig.AutoStruct):
+    def __init__(self):
+        libbig.AutoStruct.__init__(self,
+                                   { 'title' : '', 'start_time' : '', 'end_time' : '' })
+
+class EventsParser(xml.sax.ContentHandler):
+    def __init__(self):
+        self.__events = []
+
+    def startElement(self, name, attrs):
+        print "<" + name + ">"
+        print attrs.getNames() # .getValue('foo')
+
+        if name == 'entry':
+            e = Event()
+            self.__events.append(e)
+        elif len(self.__events) > 0:
+            e = self.__events[-1]
+            if name == 'title':
+                e.update({'title' : ''}) # FIXME
+            elif name == 'gd:when':
+                e.update({ 'start_time' : attrs.getValue('startTime'),
+                           'end_time' : attrs.getValue('endTime') })
+
+    def endElement(self, name):
+        print "</" + name + ">"
+
+    def characters(self, content):
+        print content
+
+    def get_events(self):
+        return self.__events
 
 class Google:
 
@@ -16,7 +49,15 @@ class Google:
         h.follow_all_redirects = True
         uri = 'http://www.google.com/calendar/feeds/' + self.__username + '@gmail.com/private/full'
         
-        return h.request(uri, "GET", headers = {})
+        headers, data = h.request(uri, "GET", headers = {})
+
+        try:
+            p = EventsParser()
+            xml.sax.parseString(data, p)
+            return p.get_events()
+        except xml.sax.SAXException, e:
+            return None
+        
         
 if __name__ == '__main__':
 
