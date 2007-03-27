5ee06862b2003336d960d67054d22ce3e10274b0
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-03-27 18:01:55 +0000

    Don't do more than 3 simultaenous HTTP requests to avoid overloading servers.

--- dumbhippo/trunk/client/bigboard/src/libbig.py
+++ dumbhippo/trunk/client/bigboard/src/libbig.py
@@ -1,7 +1,7 @@
 import os, code, sys, traceback, urllib2, logging, logging.config, StringIO, cookielib
-import re, tempfile, xml.dom.minidom
+import re, tempfile, xml.dom.minidom, urlparse, threading
 
-import cairo, gtk, gobject, threading
+import cairo, gtk, gobject
 import gnome
 
 import hippo
@@ -264,17 +264,42 @@ class AutoSignallingStruct(gobject.GObject, AutoStruct):
         if changed:
             self.emit("changed")    
     
-class AsyncHTTPFetcher:
+class AsyncHTTPFetcher(Singleton):
     """Asynchronously fetch objects over HTTP, invoking
        callbacks using the GLib main loop."""
-    def fetch(self, url, cb, errcb, cookies=None):
+   
+    def __init__(self):
         self.__logger = logging.getLogger("bigboard.AsyncHTTPFetcher")
-        self.__logger.debug('creating async HTTP request thread for %s' % (url,))
-        thread = threading.Thread(target=self._do_fetch, name="AsyncHTTPFetch", args=(url, cb, errcb, cookies))
-        thread.setDaemon(True)
-        thread.start()
         
-    def _do_fetch(self, url, cb, errcb, cookies):
+        self.__worker_count = 3
+        self.__work_lock = threading.RLock()
+        self.__work_cond = threading.Condition(self.__work_lock)
+        self.__work_queue = [] # <(str,func,func,cookies)>
+        self.__workers = set() # <Thread>
+        
+        for i in range(self.__worker_count):
+            self.__logger.debug('creating http worker thread')            
+            t = threading.Thread(target=self.__worker, name="AsyncHTTPWorker")
+            self.__workers.add(t)
+            t.setDaemon(True)
+            t.start()        
+       
+    def fetch(self, url, cb, errcb, cookies=None):        
+        self.__work_lock.acquire()
+        self.__work_queue.append((url, cb, errcb, cookies))
+        self.__work_cond.notify()
+        self.__work_lock.release()        
+        
+    def __worker(self):
+        while True:
+            self.__work_lock.acquire()
+            while len(self.__work_queue) == 0:
+                self.__work_cond.wait()
+            (url, cb, errcb, cookies) = self.__work_queue.pop(0)
+            self.__work_lock.release()            
+            self.__do_fetch(url, cb, errcb, cookies)
+
+    def __do_fetch(self, url, cb, errcb, cookies):
         self.__logger.debug("in thread fetch of %s" % (url,))
         try:
             request = urllib2.Request(url)
@@ -299,7 +324,7 @@ class URLImageCache(Singleton):
         self._cache = {}
         self._loads = {}
         self.__logger = logging.getLogger('bigboard.URLImageCache')
-        self._fetcher = AsyncHTTPFetcher()
+        self._fetcher = AsyncHTTPFetcher.getInstance()
     
     def get(self, url, cb, errcb):
         if self._cache.has_key(url): # TODO expire
