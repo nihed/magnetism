2a2946ed6f2a394cd39ffe6a7e783ca5b5f8e25a
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-04-17 16:39:15 +0000

    Override default deskbar module list.

--- dumbhippo/trunk/client/bigboard/src/stocks/search/SearchStock.py
+++ dumbhippo/trunk/client/bigboard/src/stocks/search/SearchStock.py
@@ -1,6 +1,6 @@
 import logging, os
 
-import gobject, gtk, gnomeapplet
+import gobject, gtk, gnomeapplet, gconf
 
 try:
     import deskbar, deskbar.DeskbarApplet
@@ -30,7 +30,8 @@ class SearchStock(Stock):
         
         self.__applet = gnomeapplet.Applet()
         self.__applet.get_orient = lambda: gnomeapplet.ORIENT_DOWN
-        deskbar.DeskbarApplet.DeskbarApplet(self.__applet)        
+        self.__deskbar = deskbar.DeskbarApplet.DeskbarApplet(self.__applet)
+        self.__deskbar.loader.connect("modules-loaded", self.__override_modules_loaded)
         self.__applet.reparent(self.__vbox)
         
         self.__box.append(self.__widget)
@@ -38,13 +39,21 @@ class SearchStock(Stock):
         
     def get_content(self, size):
         return size == self.SIZE_BULL and self.__box or self.__empty_box
-        
-    def __queue_search(self):
-        text = self.__input.get_property("text")
-        self._logger.debug("initiating query for %s" % (text,))
-        self.__deskbar.query(text,
-                             self.__handle_result)
-        
-    def __handle_result(self, qstring, matches):
-        self._logger.debug("got matches for %s: %s", qstring, matches)
 
+    def __override_modules_loaded(self, loader):
+        self._logger.debug("got modules loaded")        
+        gobject.idle_add(self.__idle_override_modules_loaded)
+
+    def __idle_override_modules_loaded(self):
+        self._logger.debug("idle override modules")
+        enabled_handlers = gconf.Value(gconf.VALUE_LIST)
+        def make_str_val(s):
+            v = gconf.Value(gconf.VALUE_STRING)
+            v.set_string(s)
+            return v
+        enabled_handlers.set_list_type(gconf.VALUE_STRING)
+        enabled_handlers.set_list(map(make_str_val, ['ProgramsHandler', 'MozillaBookmarksHandler', 'YahooHandler']))
+        self.__deskbar.on_config_handlers(enabled_handlers)  
+        self._logger.debug("idle override modules complete")
+
+       
