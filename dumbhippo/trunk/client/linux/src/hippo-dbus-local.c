2258be6f0d7e14ef02bbc8b0553f72bef781d1e6
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-06-22 21:19:12 +0000

    get the local presence info from local-export-daemon and sync it to the im system

--- dumbhippo/trunk/client/linux/src/hippo-dbus-local.c
+++ dumbhippo/trunk/client/linux/src/hippo-dbus-local.c
@@ -13,6 +13,181 @@ typedef struct {
 
 } LocalState;
 
+typedef struct {
+    const char *key;
+    const char *value;
+} DictStringEntry;
+
+static gboolean
+dict_extract_string_values(const DBusMessageIter *orig_dict_iter,
+                           DictStringEntry       *entries)
+{
+    DBusMessageIter dict_iter;
+
+    {
+        int i;
+        for (i = 0; entries[i].key != NULL; ++i) {
+            entries[i].value = NULL;
+        }
+    }
+    
+    dict_iter = *orig_dict_iter;
+    while (dbus_message_iter_get_arg_type(&dict_iter) == DBUS_TYPE_DICT_ENTRY) {
+        DBusMessageIter entry_iter;
+        DBusMessageIter variant_iter;
+        const char *key;
+        const char *value;
+        
+        dbus_message_iter_recurse(&dict_iter, &entry_iter);
+
+        key = NULL;
+        dbus_message_iter_get_basic(&entry_iter, &key);
+        dbus_message_iter_next(&entry_iter);
+
+        g_assert(dbus_message_iter_get_arg_type(&entry_iter) == DBUS_TYPE_VARIANT);
+        dbus_message_iter_recurse(&entry_iter, &variant_iter);
+
+        if (dbus_message_iter_get_arg_type(&variant_iter) == DBUS_TYPE_STRING) {
+            dbus_message_iter_get_basic(&variant_iter, &value);
+        } else {
+            value = NULL;
+        }
+        
+        if (value != NULL) {
+            int i;
+            for (i = 0; entries[i].key != NULL; ++i) {
+                if (strcmp(key, entries[i].key) == 0) {
+                    entries[i].value = value;
+                    break;
+                }
+            }
+        }
+            
+        dbus_message_iter_next(&dict_iter);
+    }
+
+    /* We failed if we didn't find all the keys we were expecting */
+    {
+        int i;
+        for (i = 0; entries[i].key != NULL; ++i) {
+            if (entries[i].value == NULL) {
+                g_warning("Missing property '%s'", entries[i].key);
+                return FALSE;
+            }
+        }
+    }
+    
+    return TRUE;
+}
+
+static gboolean
+read_info(DBusMessageIter *struct_iter,
+          char           **machine_id_p,
+          char           **session_id_p,
+          char           **user_resource_id_p)
+{
+    DictStringEntry session_entries[3];
+    DictStringEntry info_entries[2];
+    DBusMessageIter dict_iter;
+    
+    session_entries[0].key = "machine";
+    session_entries[1].key = "session";
+    session_entries[2].key = NULL;
+
+    dbus_message_iter_recurse(struct_iter, &dict_iter);
+
+    if (!dict_extract_string_values(&dict_iter, &session_entries[0]))
+        return FALSE;
+
+    dbus_message_iter_next(struct_iter);
+
+    info_entries[0].key = "user_resource_id";
+    info_entries[1].key = NULL;
+    
+    dbus_message_iter_recurse(struct_iter, &dict_iter);
+    
+    if (!dict_extract_string_values(&dict_iter, &info_entries[0]))
+        return FALSE;
+
+    if (machine_id_p)
+        *machine_id_p = g_strdup(session_entries[0].value);
+    if (session_id_p)
+        *session_id_p = g_strdup(session_entries[1].value);
+    if (user_resource_id_p)
+        *user_resource_id_p = g_strdup(info_entries[0].value);
+    
+    return TRUE;
+}
+
+static void
+update_info(HippoNotificationSet *notifications,
+            DBusMessageIter      *struct_iter)
+{
+    char *machine_id;
+    char *session_id;
+    char *user_resource_id;
+
+    machine_id = NULL;
+    session_id = NULL;
+    user_resource_id = NULL;
+    if (!read_info(struct_iter, &machine_id, &session_id,
+                   &user_resource_id))
+        return;
+
+    hippo_dbus_im_update_buddy(notifications, user_resource_id,
+                               "mugshot-local", user_resource_id, TRUE,
+                               "Around");
+}
+
+static gboolean
+get_info_from_all_sessions(HippoDBusProxy *proxy)
+{
+    DBusMessage *reply;
+    DBusError derror;
+    dbus_bool_t retval;
+    const char *info_name;
+
+    info_name = "org.mugshot.MugshotInfo";
+    dbus_error_init(&derror);    
+    reply = hippo_dbus_proxy_call_method_sync(proxy, "GetInfoFromAllSessions",
+                                              &derror,
+                                              DBUS_TYPE_STRING, &info_name,
+                                              DBUS_TYPE_INVALID);
+
+    retval = hippo_dbus_proxy_finish_method_call_keeping_reply(reply, "GetInfoFromAllSessions", &derror,
+                                                               DBUS_TYPE_INVALID);
+
+    if (retval) {
+        if (!dbus_message_has_signature(reply, "a(a{sv}a{sv})")) {
+            g_warning("Bad signature on GetInfoFromAllSessions reply");
+            retval = FALSE;
+        } else {
+            DBusMessageIter iter, array_iter;
+            HippoNotificationSet *notifications;
+            
+            notifications = hippo_dbus_im_start_notifications();    
+            
+            dbus_message_iter_init(reply, &iter);
+            dbus_message_iter_recurse(&iter, &array_iter);
+            while (dbus_message_iter_get_arg_type(&array_iter) != DBUS_TYPE_INVALID) {
+                DBusMessageIter struct_iter;
+                dbus_message_iter_recurse(&array_iter, &struct_iter);
+                
+                update_info(notifications, &struct_iter);
+                
+                dbus_message_iter_next(&array_iter);
+            }
+
+            hippo_dbus_im_send_notifications(notifications);
+        }
+    }
+
+    if (reply != NULL)
+        dbus_message_unref(reply);
+
+    return retval;
+}
+
 static dbus_bool_t
 append_string_pair(DBusMessageIter *dict_iter,
                    const char      *key,
@@ -40,6 +215,35 @@ append_string_pair(DBusMessageIter *dict_iter,
         return FALSE;
 
     return TRUE;
+}
+
+static void
+handle_info_changed(DBusConnection *connection,
+                    DBusMessage    *message,
+                    void           *data)
+{
+    DBusMessageIter iter, struct_iter;
+    HippoNotificationSet *notifications;
+    const char *name;
+    
+    if (!dbus_message_has_signature(message, "s(a{sv}a{sv})"))
+        return;
+    
+    dbus_message_iter_init(message, &iter);
+
+    name = NULL;
+    dbus_message_iter_get_basic(&iter, &name);
+
+    if (!(name && strcmp(name, "org.mugshot.MugshotInfo") == 0)) {
+        return;
+    }
+
+    dbus_message_iter_next(&iter);
+    dbus_message_iter_recurse(&iter, &struct_iter);
+
+    notifications = hippo_dbus_im_start_notifications();    
+    update_info(notifications, &struct_iter);
+    hippo_dbus_im_send_notifications(notifications);
 }
 
 /* FIXME we need to subscribe to changes on this and update our info when it changes */
@@ -99,6 +303,15 @@ handle_service_available(DBusConnection *connection,
     hippo_dbus_proxy_call_method_async_appender(proxy, "AddInfoToOurSession",
                                                 NULL, NULL, NULL,
                                                 append_mugshot_info, NULL);
+
+    /* blocking call to get all the latest stuff */
+    get_info_from_all_sessions(proxy);
+
+    /* FIXME for now we have no way to learn about removals, and we don't
+     * handle them
+     */
+    
+    hippo_dbus_proxy_unref(proxy);
 }
 
 static void
@@ -111,6 +324,10 @@ handle_service_unavailable(DBusConnection *connection,
 }
 
 static const HippoDBusSignalTracker signal_handlers[] = {
+    /* it would be slightly nicer if we also matched arg0=org.mugshot.MugshotInfo
+     * on this signal
+     */
+    { "org.freedesktop.od.LocalExport", "InfoChanged", handle_info_changed },
     { NULL, NULL, NULL }
 };
 
