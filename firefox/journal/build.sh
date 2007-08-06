e373eccd2772a0b17569f47febc10d0a74e3a920
clarkbw <clarkbw@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-08-06 21:22:51 +0000

    update the build script to exclude svn directories, see source http://kb.mozillazine.org/Bash_build_script

--- firefox/journal/build.sh
+++ firefox/journal/build.sh
@@ -64,14 +64,14 @@ $BEFORE_BUILD
 
 mkdir --parents --verbose $TMP_DIR/chrome
 
-# generate the JAR file, excluding CVS and temporary files
+# generate the JAR file, excluding CVS, SVN, and temporary files
 JAR_FILE=$TMP_DIR/chrome/$APP_NAME.jar
 echo "Generating $JAR_FILE..."
 for CHROME_SUBDIR in $CHROME_PROVIDERS; do
-  find $CHROME_SUBDIR -path '*CVS*' -prune -o -type f -print | grep -v \~ >> files
+  find $CHROME_SUBDIR \( -path '*CVS*' -o -path '*.svn*' \) -prune -o -type f -print | grep -v \~ >> files
 done
 
-zip -0 -r $JAR_FILE `cat files`
+zip -0 -r $JAR_FILE -@ < files
 # The following statement should be used instead if you don't wish to use the JAR file
 #cp --verbose --parents `cat files` $TMP_DIR/chrome
 
@@ -79,7 +79,7 @@ zip -0 -r $JAR_FILE `cat files`
 echo "Copying various files to $TMP_DIR folder..."
 for DIR in $ROOT_DIRS; do
   mkdir $TMP_DIR/$DIR
-  FILES="`find $DIR -path '*CVS*' -prune -o -type f -print | grep -v \~`"
+  FILES="`find $DIR \( -path '*CVS*' -o -path '*.svn*' \) -prune -o -type f -print | grep -v \~`"
   echo $FILES >> files
   cp --verbose --parents $FILES $TMP_DIR
 done
