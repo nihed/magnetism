def678c3d33946f4f4aeb81291114baa4a0ba7cd
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-08-27 17:25:12 +0000

    give up on applet for now, too broken

--- online-desktop/trunk/pyonlinedesktop/odpanel.py
+++ online-desktop/trunk/pyonlinedesktop/odpanel.py
@@ -6,7 +6,7 @@ PANEL_ID = 'bottom_panel_' + NAME
 
 def setup_panels():
     gnomepanel.create_toplevel(PANEL_ID)
-    gnomepanel.add_applet(PANEL_ID, 'bigboard_' + NAME, 'BigBoard_Applet', 1)
+    #gnomepanel.add_applet(PANEL_ID, 'bigboard_' + NAME, 'BigBoard_Applet', 1)
     gnomepanel.add_applet(PANEL_ID, 'windowlist_' + NAME, 'GNOME_WindowListApplet', 2)
     gnomepanel.add_applet(PANEL_ID, 'clock_' + NAME, 'GNOME_ClockApplet', 1, right=True)
     gnomepanel.add_applet(PANEL_ID, 'tray_' + NAME, 'GNOME_SystemTrayApplet', 0, right=True)
