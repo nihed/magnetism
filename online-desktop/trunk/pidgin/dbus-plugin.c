962eec5c0534d88a9d319a4ba96808e7eab21991
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-09-14 22:06:53 +0000

    add basic buddy getting and change notification

--- online-desktop/trunk/pidgin/dbus-plugin.c
+++ online-desktop/trunk/pidgin/dbus-plugin.c
@@ -5,6 +5,7 @@
 
 #include <plugin.h>
 #include <version.h>
+#include <blist.h>
 
 #include "hippo-dbus-helper.h"
 #include <dbus/dbus-glib-lowlevel.h>
@@ -17,6 +18,92 @@ typedef struct {
     DBusConnection *connection;
 
 } PluginData;
+
+
+static void
+append_basic_entry(DBusMessageIter *dict_iter,
+                   const char      *key,
+                   int              type,
+                   const void      *value)
+{
+    DBusMessageIter entry_iter, variant_iter;
+    char type_str[2];
+    type_str[0] = type;
+    type_str[1] = '\0';
+
+    dbus_message_iter_open_container(dict_iter, DBUS_TYPE_DICT_ENTRY, NULL, &entry_iter);
+    
+    dbus_message_iter_append_basic(&entry_iter, DBUS_TYPE_STRING, &key);
+
+    dbus_message_iter_open_container(&entry_iter, DBUS_TYPE_VARIANT, type_str, &variant_iter);
+    
+    dbus_message_iter_append_basic(&variant_iter, type, value);
+
+    dbus_message_iter_close_container(&entry_iter, &variant_iter);
+
+    dbus_message_iter_close_container(dict_iter, &entry_iter);
+}
+
+static void
+append_buddy(DBusMessageIter        *append_iter,
+             PurpleBuddy            *buddy)
+{
+    DBusMessageIter dict_iter;
+    dbus_bool_t is_online;
+    
+    dbus_message_iter_open_container(append_iter, DBUS_TYPE_ARRAY, "{sv}", &dict_iter);
+    
+    append_basic_entry(&dict_iter, "name", DBUS_TYPE_STRING, &buddy->name);
+
+    is_online = PURPLE_BUDDY_IS_ONLINE(buddy);
+    append_basic_entry(&dict_iter, "online", DBUS_TYPE_BOOLEAN, &is_online);
+    
+#if 0
+    /* FIXME */
+    append_basic_entry(&dict_iter, "protocol", DBUS_TYPE_STRING, &buddy->protocol);
+    append_basic_entry(&dict_iter, "status", DBUS_TYPE_STRING, &buddy->status);
+#endif
+    
+    dbus_message_iter_close_container(append_iter, &dict_iter);
+}
+
+static void
+recurse_finding_buddies(PurpleBlistNode *node,
+                        GSList         **buddies_p)
+{
+    PurpleBlistNode *iter;
+    
+    for (iter = node; iter != NULL; iter = iter->next) {
+
+        if (iter->type == PURPLE_BLIST_BUDDY_NODE) {
+            *buddies_p = g_slist_prepend(*buddies_p, iter);
+        }
+
+        if (iter->child) {
+            recurse_finding_buddies(iter->child, buddies_p);
+        }
+    }
+}
+
+static void
+append_all_buddies(DBusMessageIter *append_iter)
+{
+    PurpleBlistNode *root;
+    GSList *buddies;
+    
+    root = purple_blist_get_root();
+    
+    buddies = NULL;
+
+    recurse_finding_buddies(root, &buddies);
+
+    while (buddies != NULL) {
+        PurpleBuddy *buddy = buddies->data;
+        buddies = g_slist_remove(buddies, buddies->data);
+
+        append_buddy(append_iter, buddy);
+    }
+}
 
 static DBusMessage*
 handle_get_buddies(void            *object,
@@ -36,7 +123,7 @@ handle_get_buddies(void            *object,
     /* open an array of dict */
     dbus_message_iter_open_container(&iter, DBUS_TYPE_ARRAY, "a{sv}", &array_iter);
 
-    /* FIXME write the buddies */
+    append_all_buddies(&array_iter);
     
     dbus_message_iter_close_container(&iter, &array_iter);
 
@@ -52,19 +139,121 @@ static const HippoDBusMember im_members[] = {
 };
 
 static void
-hippo_dbus_im_emit_buddy_list_changed (DBusConnection *connection)
+emit_buddy_list_changed (DBusConnection *connection)
 {
     hippo_dbus_helper_emit_signal(connection, HIPPO_DBUS_IM_PATH, HIPPO_DBUS_IM_INTERFACE,
                                   "BuddyListChanged", DBUS_TYPE_INVALID);
 }
 
+static dbus_bool_t
+buddy_appender(DBusMessage *message,
+               void        *data)
+{
+    DBusMessageIter iter;
+    PurpleBuddy *buddy;
+
+    buddy = data;
+    
+    dbus_message_iter_init_append(message, &iter);
+    
+    append_buddy(&iter, buddy);
+    
+    return TRUE;
+}
+
+static void
+emit_buddy_changed (DBusConnection *connection,
+                    PurpleBuddy    *buddy)
+{
+    hippo_dbus_helper_emit_signal_appender(connection, HIPPO_DBUS_IM_PATH, HIPPO_DBUS_IM_INTERFACE,
+                                           "BuddyChanged", buddy_appender, buddy);
+}
+
+
+/* means the buddy was added to the persistent list, not that it was added
+ * to the buddy list object in this process.
+ */
+static void
+on_buddy_added(PurpleBuddy *buddy,
+               void        *data)
+{
+    PluginData *pd = data;
+    
+    emit_buddy_list_changed(pd->connection);
+}
+
+/* means the buddy was removed from the persistent list, not that it was removed
+ * from the buddy list object in this process.
+ */
+static void
+on_buddy_removed(PurpleBuddy *buddy,
+                 void        *data)
+{
+    PluginData *pd = data;
+    
+    emit_buddy_list_changed(pd->connection);
+}
+
+static void
+on_buddy_icon_changed(PurpleBuddy *buddy,
+                      void        *data)
+{
+    PluginData *pd = data;
+
+    /* g_printerr("buddy icon changed\n"); */
+    
+    emit_buddy_changed(pd->connection, buddy);
+}
+
+static void
+on_buddy_status_changed(PurpleBuddy *buddy,
+                        void        *some_pidgin_status_thingy,
+                        void        *some_pidgin_status_thingy2,
+                        void        *data)
+{
+    PluginData *pd = data;
+
+    /* g_printerr("buddy status changed\n"); */
+    
+    emit_buddy_changed(pd->connection, buddy);
+}
+
+static void
+on_buddy_signed_on(PurpleBuddy *buddy,
+                   void        *data)
+{
+    PluginData *pd = data;
+
+    /* g_printerr("buddy signed on\n"); */
+    
+    emit_buddy_changed(pd->connection, buddy);
+}
+
+static void
+on_buddy_signed_off(PurpleBuddy *buddy,
+                    void        *data)
+{
+    PluginData *pd = data;
+
+    /* g_printerr("buddy signed off\n"); */
+    
+    emit_buddy_changed(pd->connection, buddy);
+}
+
+static void*
+get_signal_handle(void)
+{
+    static int signal_handle;
+
+    return &signal_handle;
+}
 
 static gboolean
 plugin_load(PurplePlugin *plugin)
 {
     PluginData *pd;
     DBusConnection *connection;
-
+    
     connection = dbus_bus_get(DBUS_BUS_SESSION, NULL);
     if (connection == NULL)
         return FALSE;
@@ -84,22 +273,46 @@ plugin_load(PurplePlugin *plugin)
     hippo_dbus_helper_register_object(connection, HIPPO_DBUS_IM_PATH,
                                       pd,
                                       HIPPO_DBUS_IM_INTERFACE, NULL);
+
+    purple_signal_connect(purple_blist_get_handle(), "buddy-added",
+                          get_signal_handle(),
+                          PURPLE_CALLBACK(on_buddy_added), pd);
+    
+    purple_signal_connect(purple_blist_get_handle(), "buddy-removed",
+                          get_signal_handle(),
+                          PURPLE_CALLBACK(on_buddy_removed), pd);
+    
+    purple_signal_connect(purple_blist_get_handle(), "buddy-icon-changed",
+                          get_signal_handle(),
+                          PURPLE_CALLBACK(on_buddy_icon_changed), pd);
+    
+    purple_signal_connect(purple_blist_get_handle(), "buddy-status-changed",
+                          get_signal_handle(),
+                          PURPLE_CALLBACK(on_buddy_status_changed), pd);
+
+    purple_signal_connect(purple_blist_get_handle(), "buddy-signed-on",
+                          get_signal_handle(),
+                          PURPLE_CALLBACK(on_buddy_signed_on), pd);
+
+    purple_signal_connect(purple_blist_get_handle(), "buddy-signed-off",
+                          get_signal_handle(),
+                          PURPLE_CALLBACK(on_buddy_signed_off), pd);
     
     return TRUE;
 }
 
-/* this should be idempotent since I'm not sure
- * when pidgin calls this vs. unload()
- */
+/* Pidgin calls unload() then destroy() apparently */
 static void
 plugin_destroy(PurplePlugin *plugin)
 {
     PluginData *pd;
-
+    
     pd = plugin->extra;
 
     if (pd != NULL) {
         hippo_dbus_helper_unregister_object(pd->connection, HIPPO_DBUS_IM_PATH);
+
+        purple_signals_disconnect_by_handle(get_signal_handle());
         
         dbus_connection_unref(pd->connection);
         g_free(pd);
@@ -110,14 +323,29 @@ plugin_destroy(PurplePlugin *plugin)
 static gboolean
 plugin_unload(PurplePlugin *plugin)
 {
+    /* I guess Pidgin does this for us, but since we set up in load() I feel
+     * like we should clean up in unload...
+     */
     plugin_destroy(plugin);
     return TRUE;
+}
+
+static gboolean
+self_load_idle(void *data)
+{
+    PurplePlugin *plugin = data;
+    purple_plugin_load(plugin);
+    return FALSE;
 }
 
 static void
 plugin_init(PurplePlugin *plugin)
 {
-    
+
+    /* Lame hack to avoid user having to manually enable this plugin;
+     * we'll see how it works out...
+     */
+    g_idle_add(self_load_idle, plugin);
 }
 
 static PurplePluginInfo plugin_info = {
@@ -126,7 +354,7 @@ static PurplePluginInfo plugin_info = {
     PURPLE_MINOR_VERSION,
     PURPLE_PLUGIN_STANDARD,
     NULL,
-    PURPLE_PLUGIN_FLAG_INVISIBLE,
+    PURPLE_PLUGIN_FLAG_INVISIBLE, /* since we self-enable */
     NULL,
     PURPLE_PRIORITY_LOWEST, /* load after other plugins (I think that's what this is) */
     "dbus-api-plugin",
