56334c65fc5287d903027589a516e35f71c1b1ee
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-09-17 18:58:17 +0000

    implement getting buddy icons from Pidgin

--- online-desktop/trunk/pidgin/dbus-plugin.c
+++ online-desktop/trunk/pidgin/dbus-plugin.c
@@ -8,6 +8,8 @@
 #include <plugin.h>
 #include <version.h>
 #include <blist.h>
+#include <util.h>
+#include <cipher.h>
 
 #include "hippo-dbus-helper.h"
 #include <dbus/dbus-glib-lowlevel.h>
@@ -15,12 +17,102 @@
 #define HIPPO_DBUS_IM_INTERFACE "org.freedesktop.od.IM"
 #define HIPPO_DBUS_IM_PATH "/org/freedesktop/od/im"
 
+typedef struct {
+    char *hash;
+    /* We can't just hold a ref to a PurpleBuddyIcon because
+     * of purple_buddy_icon_set_data().
+     */
+    GString *data;
+} IconData;
 
 typedef struct {
     DBusConnection *connection;
-
+    GHashTable *icons;
 } PluginData;
 
+static char*
+hash_icon(const void *image_data,
+          size_t      image_len)
+{
+    PurpleCipherContext *context;
+    gchar digest[41];
+    
+    context = purple_cipher_context_new_by_name("sha1", NULL);
+    if (context == NULL) {
+        g_warning("Could not find SHA-1 cipher");
+        g_assert_not_reached();
+    }
+    
+    /* Hash the image data */
+    purple_cipher_context_append(context, image_data, image_len);
+    if (!purple_cipher_context_digest_to_str(context, sizeof(digest), digest, NULL)) {
+        g_warning("Could not compute SHA-1 cipher");
+        g_assert_not_reached();
+    }
+    purple_cipher_context_destroy(context);
+
+    g_assert(digest[40] == '\0');
+    g_assert(strlen(digest) == 40);
+    return g_strdup(digest);
+}
+
+static IconData*
+lookup_icon_data(PluginData      *pd,
+                 const char      *hash)
+{
+    return g_hash_table_lookup(pd->icons, hash);
+}
+
+static IconData*
+ensure_icon_data(PluginData      *pd,
+                 PurpleBuddyIcon *icon)
+{
+    IconData *id;
+    const void *image_data;
+    size_t image_len;
+    char *hash;    
+    
+    image_len = 0;
+    image_data = purple_buddy_icon_get_data(icon, &image_len);
+
+    hash = hash_icon(image_data, image_len);
+    id = g_hash_table_lookup(pd->icons, hash);
+    if (id != NULL) {
+        g_free(hash);
+        return id;
+    }
+
+    /* We store a copy of purple_buddy_icon_get_data() data because
+     * there is a purple_buddy_icon_set_data() and it isn't clear
+     * to me how we'd reliably get change notification when it's used
+     * in order to re-hash.
+     */
+    
+    id = g_new0(IconData, 1);
+    id->hash = hash;
+    id->data = g_string_new_len(image_data, image_len);
+
+    g_hash_table_replace(pd->icons, id->hash, id);
+
+    /* FIXME the only "robust" way to remove stuff from this icon hash
+     * without precise semantics from libpurple would be to run a periodic
+     * GC; i.e. walk all the buddies and see if any icons are no longer
+     * present.
+     * 
+     * For now our goal is just to work, and we'll have to refine later.
+     */
+    
+    return id;
+}
+
+static void
+icon_data_free(void *data)
+{
+    IconData *id = data;
+    g_free(id->hash);
+    g_string_free(id->data, TRUE);
+    g_free(id);
+}
 
 static void
 append_basic_entry(DBusMessageIter *dict_iter,
@@ -47,13 +139,15 @@ append_basic_entry(DBusMessageIter *dict_iter,
 }
 
 static void
-append_buddy(DBusMessageIter        *append_iter,
+append_buddy(PluginData             *pd,
+             DBusMessageIter        *append_iter,
              PurpleBuddy            *buddy)
 {
     DBusMessageIter dict_iter;
     dbus_bool_t is_online;
     const char *protocol;
-
+    PurpleBuddyIcon *icon;
+    
     dbus_message_iter_open_container(append_iter, DBUS_TYPE_ARRAY, "{sv}", &dict_iter);
     
     append_basic_entry(&dict_iter, "name", DBUS_TYPE_STRING, &buddy->name);
@@ -63,10 +157,21 @@ append_buddy(DBusMessageIter        *append_iter,
 
     if (strcmp(buddy->account->protocol_id, "prpl-aim") == 0)
         protocol = "aim";
+    else if (strcmp(buddy->account->protocol_id, "prpl-jabber") == 0)
+        protocol = "xmpp";
     else
         protocol = "unknown";
 
     append_basic_entry(&dict_iter, "protocol", DBUS_TYPE_STRING, &protocol);
+
+    icon = purple_buddy_get_icon(buddy);
+    if (icon) {
+        IconData *id;
+
+        id = ensure_icon_data(pd, icon);
+
+        append_basic_entry(&dict_iter, "icon", DBUS_TYPE_STRING, &id->hash);
+    }
     
 #if 0
     /* FIXME */
@@ -95,7 +200,8 @@ recurse_finding_buddies(PurpleBlistNode *node,
 }
 
 static void
-append_all_buddies(DBusMessageIter *append_iter)
+append_all_buddies(PluginData      *pd,
+                   DBusMessageIter *append_iter)
 {
     PurpleBlistNode *root;
     GSList *buddies;
@@ -110,7 +216,7 @@ append_all_buddies(DBusMessageIter *append_iter)
         PurpleBuddy *buddy = buddies->data;
         buddies = g_slist_remove(buddies, buddies->data);
 
-        append_buddy(append_iter, buddy);
+        append_buddy(pd, append_iter, buddy);
     }
 }
 
@@ -132,7 +238,7 @@ handle_get_buddies(void            *object,
     /* open an array of dict */
     dbus_message_iter_open_container(&iter, DBUS_TYPE_ARRAY, "a{sv}", &array_iter);
 
-    append_all_buddies(&array_iter);
+    append_all_buddies(pd, &array_iter);
     
     dbus_message_iter_close_container(&iter, &array_iter);
 
@@ -144,17 +250,58 @@ handle_get_icon(void            *object,
                 DBusMessage     *message,
                 DBusError       *error)
 {
-    /* FIXME */
+    DBusMessage *reply;
+    DBusMessageIter append_iter;
+    DBusMessageIter array_iter;
+    const char *requested_id;
+    IconData *id;
+    PluginData *pd;
+    char *content_type;
+
+    pd = object;
     
-    return dbus_message_new_method_return(message);
+    requested_id = NULL;
+    if (!dbus_message_get_args(message, error,
+                               DBUS_TYPE_STRING, &requested_id,
+                               DBUS_TYPE_INVALID))
+        return NULL;
+
+    id = lookup_icon_data(pd, requested_id);
+    if (id == NULL) {
+        dbus_set_error(error, DBUS_ERROR_FAILED,
+                       "Unknown icon ID");
+        return NULL;
+    }
+    
+    reply = dbus_message_new_method_return(message); 
+    dbus_message_iter_init_append(reply, &append_iter);
+
+    content_type = g_strdup_printf("image/%s",
+                                   purple_util_get_image_extension(id->data->str,
+                                                                   id->data->len));
+    
+    dbus_message_iter_append_basic(&append_iter, DBUS_TYPE_STRING, &content_type);
+    g_free(content_type);
+
+    dbus_message_iter_open_container(&append_iter, DBUS_TYPE_ARRAY, "y", &array_iter);
+    dbus_message_iter_append_fixed_array(&array_iter, DBUS_TYPE_BYTE, &id->data->str, id->data->len);
+    dbus_message_iter_close_container(&append_iter, &array_iter);
+    
+    return reply;
 }
 
+/* This interface is the same one used in the "Online Desktop Engine" aggregator
+ * thing, keep them in sync.
+ */
 static const HippoDBusMember im_members[] = {
     { HIPPO_DBUS_MEMBER_METHOD, "GetBuddyList", "", "aa{sv}", handle_get_buddies },
-    /* args are "s" icon ID, and returns "ay" the icon in PNG or other common format.
-     * the icon ID would be in the key-value dict for a buddy, under key "icon"
+
+    /* args are "s" icon ID, and returns "s" content-type and "ay" the
+     * icon in PNG or other common format.  the icon ID would be in
+     * the key-value dict for a buddy, under key "icon"
      */
-    { HIPPO_DBUS_MEMBER_METHOD, "GetIcon", "s", "ay", handle_get_icon },
+    
+    { HIPPO_DBUS_MEMBER_METHOD, "GetIcon", "s", "say", handle_get_icon },
     { HIPPO_DBUS_MEMBER_SIGNAL, "BuddyListChanged", "", "", NULL },
     { HIPPO_DBUS_MEMBER_SIGNAL, "BuddyChanged", "", "a{sv}", NULL },
     { 0, NULL }
@@ -167,28 +314,37 @@ emit_buddy_list_changed (DBusConnection *connection)
                                   "BuddyListChanged", DBUS_TYPE_INVALID);
 }
 
+typedef struct {
+    PluginData *pd;
+    PurpleBuddy *buddy;
+} BuddyAppenderData;
+
 static dbus_bool_t
 buddy_appender(DBusMessage *message,
                void        *data)
 {
     DBusMessageIter iter;
-    PurpleBuddy *buddy;
+    BuddyAppenderData *bad;
 
-    buddy = data;
-    
+    bad = data;
+
     dbus_message_iter_init_append(message, &iter);
     
-    append_buddy(&iter, buddy);
+    append_buddy(bad->pd, &iter, bad->buddy);
     
     return TRUE;
 }
 
 static void
-emit_buddy_changed (DBusConnection *connection,
+emit_buddy_changed (PluginData     *pd,
                     PurpleBuddy    *buddy)
 {
-    hippo_dbus_helper_emit_signal_appender(connection, HIPPO_DBUS_IM_PATH, HIPPO_DBUS_IM_INTERFACE,
-                                           "BuddyChanged", buddy_appender, buddy);
+    BuddyAppenderData bad;
+
+    bad.pd = pd;
+    bad.buddy = buddy;
+    hippo_dbus_helper_emit_signal_appender(pd->connection, HIPPO_DBUS_IM_PATH, HIPPO_DBUS_IM_INTERFACE,
+                                           "BuddyChanged", buddy_appender, &bad);
 }
 
 
@@ -224,7 +380,7 @@ on_buddy_icon_changed(PurpleBuddy *buddy,
 
     /* g_printerr("buddy icon changed\n"); */
     
-    emit_buddy_changed(pd->connection, buddy);
+    emit_buddy_changed(pd, buddy);
 }
 
 static void
@@ -237,7 +393,7 @@ on_buddy_status_changed(PurpleBuddy *buddy,
 
     /* g_printerr("buddy status changed\n"); */
     
-    emit_buddy_changed(pd->connection, buddy);
+    emit_buddy_changed(pd, buddy);
 }
 
 static void
@@ -248,7 +404,7 @@ on_buddy_signed_on(PurpleBuddy *buddy,
 
     /* g_printerr("buddy signed on\n"); */
     
-    emit_buddy_changed(pd->connection, buddy);
+    emit_buddy_changed(pd, buddy);
 }
 
 static void
@@ -259,7 +415,7 @@ on_buddy_signed_off(PurpleBuddy *buddy,
 
     /* g_printerr("buddy signed off\n"); */
     
-    emit_buddy_changed(pd->connection, buddy);
+    emit_buddy_changed(pd, buddy);
 }
 
 static void*
@@ -284,6 +440,9 @@ plugin_load(PurplePlugin *plugin)
     pd->connection = connection;
     dbus_connection_ref(pd->connection);
 
+    pd->icons = g_hash_table_new_full(g_str_hash, g_str_equal, NULL,
+                                      icon_data_free);
+    
     plugin->extra = pd;
 
     /* in case nobody has done it yet */
