2207f79c2917ae5bcc89b07e1264168a2ed5d61c
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-10-02 15:13:04 +0000

    Add a volume control.
    Be idempotent if we already own the panel config.

--- online-desktop/trunk/pyonlinedesktop/odpanel.py
+++ online-desktop/trunk/pyonlinedesktop/odpanel.py
@@ -5,9 +5,13 @@ NAME = 'OD'
 PANEL_ID = 'bottom_panel_' + NAME
 
 def setup_panels():
+    toplevels = gnomepanel.get_toplevels()
+    if toplevels == [PANEL_ID]:
+        return
     gnomepanel.create_toplevel(PANEL_ID)
     gnomepanel.add_applet(PANEL_ID, 'bigboard_' + NAME, 'GNOME_OnlineDesktop_BigBoardButtonApplet', 1)
     gnomepanel.add_applet(PANEL_ID, 'windowlist_' + NAME, 'GNOME_WindowListApplet', 2)
+    gnomepanel.add_applet(PANEL_ID, 'mixer_' + NAME, 'GNOME_MixerApplet', 1, right=True)    
     gnomepanel.add_applet(PANEL_ID, 'clock_' + NAME, 'GNOME_ClockApplet', 1, right=True)
     gnomepanel.add_applet(PANEL_ID, 'tray_' + NAME, 'GNOME_SystemTrayApplet', 0, right=True)
     gnomepanel.set_toplevels([PANEL_ID])
