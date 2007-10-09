40a6fff99ad336a7ffffb3751a7899f2fbde5b7d
hp <hp@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-10-09 21:40:42 +0000

    disable install link for app if we are not connected, simple hack until we fix this for real

--- bigboard/trunk/bigboard/stocks/apps/apps_widgets.py
+++ bigboard/trunk/bigboard/stocks/apps/apps_widgets.py
@@ -2,6 +2,7 @@ import logging, time
 
 import gmenu, gobject, pango, gnomedesktop
 import hippo
+import bigboard.globals as globals
 
 from bigboard.big_widgets import CanvasMugshotURLImage, PhotoContentItem, CanvasHBox, CanvasVBox, ActionLink
 
@@ -78,7 +79,8 @@ class AppDisplay(PhotoContentItem):
         self.__title.set_property("text", self.__app.get_name())
         if self.__app.is_installed() or self.__app_location == AppLocation.DESCRIPTION_HEADER:
             self.__subtitle.set_property("text", self.__app.get_generic_name() or self.__app.get_tooltip() or self.__app.get_comment())
-        elif self.__app_location == AppLocation.STOCK:
+        ## for now, install won't work if not connected
+        elif self.__app_location == AppLocation.STOCK and globals.get_data_model().connected:
             self.__subtitle.set_property('text', "(Click to Install)")
         else:
             self.__subtitle.set_property('text', "(Not Installed)")
