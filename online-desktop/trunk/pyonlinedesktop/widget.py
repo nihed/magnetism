099d476c2e682905566814fd235ef6519b5b0ad1
walters <walters@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2007-10-15 20:20:49 +0000

    Move gtkmozembed/gadget stuff into ggadget.py.

--- online-desktop/trunk/pyonlinedesktop/widget.py
+++ online-desktop/trunk/pyonlinedesktop/widget.py
@@ -5,9 +5,7 @@ import cookielib
 import xml.etree.ElementTree
 from StringIO import StringIO
 
-import gobject,gtk,gnomevfs,gtkmozembed
-
-import pyonlinedesktop.mozembed_wrap as mozembed_wrap
+import gobject,gtk,gnomevfs
 
 _logger = logging.getLogger("od.WidgetSystem")
 
@@ -103,86 +101,3 @@ _IG_MiniMessage.prototype = {
             result.write(',\n')
         result.write('''}\n''')
         return result.getvalue()
-  
-class Widget(gtk.VBox):
-
-    def __init__(self, url, env):
-        super(Widget, self).__init__()
-      
-        f = gtk.Frame()
-        self.__moz = mozembed_wrap.MozClient()
-        _logger.debug("Reading module url %s", url)
-        self.__content = content = WidgetParser(url, urllib2.urlopen(url), env)
-        (content_type, content_data) = content.content
-        if content_type == 'html':
-            self.__moz.set_data("http://www.google.com/", content_data)
-        elif content_type == 'url':
-            self.__moz.load_url(content_data)
-        else:
-            pass
-            
-        self.__moz.connect("open-uri", self.__on_open_uri)
-        self.__moz.connect("location", self.__on_location)   
-        self.__moz.connect("new-window", self.__on_new_window)
-        f.add(self.__moz)
-        self.pack_start(f, expand=True)
-        self.__moz.show_all()
-        self.__moz.set_size_request(200, int(content.height))
-
-    def __on_temp_moz_location(self, tm, *args):
-        uri = tm.get_location()        
-        _logger.debug("got location in temp moz: %s", uri)        
-
-    def __on_new_window(self, *args):
-        _logger.debug("got new window request args: %s", args)
-        self.__temp_moz = tempmoz = gtkmozembed.MozEmbed() 
-        tempmoz.connect('location', self.__on_temp_moz_location)
-        return tempmoz
-
-    def __on_location(self, *args):
-        loc = self.__moz.get_location()
-        _logger.debug("got location %s", loc)
-
-    def __on_open_uri(self, m, uri):
-        _logger.debug("got open uri: %s", uri)
-        if self.__content.content[0] == 'url' and self.__content.content[1] == uri:
-            return False
-        if not uri.startswith("http"):
-            return False
-        _logger.debug("opening in external browser: %s", uri)
-        webbrowser.open(uri)
-        return True
-
-    def get_title(self):
-        return self.__title
-
-def main():
-    logging.basicConfig(level=logging.DEBUG)
-    oddir = os.path.expanduser('~/.od/')
-    odwidgets = os.path.join(oddir, 'widgets')
-    try:
-        os.makedirs(odwidgets)
-    except:
-        pass
-
-    from pyonlinedesktop.firefox import FirefoxProfile
-    ffp = FirefoxProfile()
-    gtkmozembed.set_profile_path(oddir, 'widgets')
-    shutil.copy(ffp.path_join('cookies.txt'), odwidgets)
-
-    widget_environ = WidgetEnvironment()
-    widget_environ['google_apps_auth_path'] = ''
-
-    win = gtk.Window()
-    vb = gtk.VBox()
-    win.set_deletable(False)
-    for url in sys.argv[1:]:
-        widget = Widget(url, widget_environ) 
-        vb.add(widget)
-    #win.set_title(widget.get_title())
-    win.add(vb)
-    win.show_all()
-    gtk.main()
-
-if __name__ == '__main__':
-    main()
