6c9025ab5970c3beed7c28e1ef0230a93d8eb1d6
otaylor <otaylor@69951b7c-304f-11de-8daa-cfecb5f7ff5b>
2008-01-21 20:54:32 +0000

    Fix compile error

--- dumbhippo/trunk/server/src/com/dumbhippo/server/impl/FeedSystemBean.java
+++ dumbhippo/trunk/server/src/com/dumbhippo/server/impl/FeedSystemBean.java
@@ -1,7 +1,5 @@
 package com.dumbhippo.server.impl;
 
-import static com.dumbhippo.server.impl.FeedSystemBean.logger;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
